d90e200fa36b205dfa856cdaa82b1624eaf86c18
Phil Ayres <phil_ayres@hms.harvard.edu>
2015-07-14 15:56:42 +0000

    Precompiled assets for release: 0.1.3
    git-svn-id: https://open.med.harvard.edu/svn/fphs-rails/trunk/railsapp@43 93467e27-a4b6-4d31-bc93-299e2bd7d087

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,10 +5,11 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << "#{Rails}/vendor/assets/fonts"
+#Rails.application.config.assets.paths << "#{Rails}/app/assets/fonts"
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( *.svg *.eot *.woff *.ttf )
+#Rails.application.config.assets.precompile += %w( *.svg *.eot *.woff *.ttf )
+#Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
