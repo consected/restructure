b003f3a84263eef4c861fe1019e4591b7f038036
Phil Ayres <phil_ayres@hms.harvard.edu>
2015-08-12 20:26:15 +0000

    added release scripts
    git-svn-id: https://open.med.harvard.edu/svn/fphs-rails/trunk/railsapp@199 93467e27-a4b6-4d31-bc93-299e2bd7d087

--- bin/fphs/compile_assets.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-FPHS_RAILS_DEVISE_SECRET_KEY='rake' FPHS_RAILS_SECRET_KEY_BASE='rake' RAILS_ENV=production rake assets:clobber
-FPHS_RAILS_DEVISE_SECRET_KEY='rake' FPHS_RAILS_SECRET_KEY_BASE='rake' RAILS_ENV=production rake assets:precompile --trace
