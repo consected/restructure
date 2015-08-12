44687c1d09b21b2e7c42042f8697e2d0eb2a70e6
Phil Ayres <phil_ayres@hms.harvard.edu>
2015-08-12 20:30:41 +0000

    Precompiled assets for release: 0.2.13
    git-svn-id: https://open.med.harvard.edu/svn/fphs-rails/trunk/railsapp@212 93467e27-a4b6-4d31-bc93-299e2bd7d087

--- fphs-scripts/prep_for_release.sh
+++ fphs-scripts/prep_for_release.sh
@@ -9,7 +9,7 @@ echo $FPHS_VERSION > version.txt
 svn commit version.txt -m "new version file created"
 svn rm --force public/assets
 svn commit public/assets -m "clean up assets for deployment"
-script/fphs/compile_assets.sh
+fphs-scripts/compile_assets.sh
 pg_dump -d fpa_development -s > db/dumps/current_schema.sql
 svn add public/assets
 svn commit -m "Precompiled assets for release: $FPHS_VERSION"
