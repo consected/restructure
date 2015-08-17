18dd8640f043791f944ee5f4f6dd1acaddb9aeed
Phil Ayres <phil_ayres@hms.harvard.edu>
2015-08-17 22:09:03 +0000

    non-JIRA better layout for long strings when viewing data - correction to metadata in panel
    git-svn-id: https://open.med.harvard.edu/svn/fphs-rails/trunk/railsapp@250 93467e27-a4b6-4d31-bc93-299e2bd7d087

--- app/assets/javascripts/_fpa_form_utils.js
+++ app/assets/javascripts/_fpa_form_utils.js
@@ -50,7 +50,7 @@ _fpa.form_utils = {
             var self = $(this);
             window.setTimeout(function(){
                 var wmax = 0;
-                var lgi = self.find('.list-group-item').not('.is-heading, .is-combo');
+                var lgi = self.find('.list-group-item').not('.is-heading, .is-combo, .record-meta');
                 var all = lgi.find('small, label');
                 all.css({display: 'inline-block', whiteSpace: 'nowrap'});
                 all.each(function(){
