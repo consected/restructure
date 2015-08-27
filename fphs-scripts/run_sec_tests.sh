e0551039a5e0b509748bc60cbf17df419469aa21
Phil Ayres <phil_ayres@hms.harvard.edu>
2015-08-27 16:46:09 +0000

    New test script as part of preparing the deployment for bundler-audit and brakeman gems
    git-svn-id: https://open.med.harvard.edu/svn/fphs-rails/trunk/railsapp@356 93467e27-a4b6-4d31-bc93-299e2bd7d087

---
+++ fphs-scripts/run_sec_tests.sh
@@ -0,0 +1,3 @@
+brakeman -o ./security/brakeman-output-$FPHS_VERSION.md
+bundle-audit update > ./security/bundle-audit-update-$FPHS_VERSION.md
+bundle-audit > ./security/bundle-audit-output-$FPHS_VERSION.md
