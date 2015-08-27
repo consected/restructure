262de1eec7d074f2822535f587f72688bf152f61
Phil Ayres <phil_ayres@hms.harvard.edu>
2015-08-27 19:38:42 +0000

    make sure the security notices really are added and commit
    git-svn-id: https://open.med.harvard.edu/svn/fphs-rails/trunk/railsapp@369 93467e27-a4b6-4d31-bc93-299e2bd7d087

--- fphs-scripts/prep_for_release.sh
+++ fphs-scripts/prep_for_release.sh
@@ -12,7 +12,8 @@ svn rm --force public/assets
 svn commit public/assets -m "clean up assets for deployment"
 fphs-scripts/compile_assets.sh
 fphs-scripts/run_sec_tests.sh 
-svn commit ./security -m "store security test results"
+svn add security/*$FPHS_VERSION*
+svn commit security -m "store security test results"
 pg_dump -d fpa_development -s > db/dumps/current_schema.sql
 svn add public/assets
 svn commit -m "Precompiled assets for release: $FPHS_VERSION"
