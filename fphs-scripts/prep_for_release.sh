c6b514b9ce1821f85db5c989bace0ddf4df64664
Phil Ayres <phil_ayres@hms.harvard.edu>
2015-11-17 17:00:41 +0000

    Minor pre-production bug fixes
    git-svn-id: https://open.med.harvard.edu/svn/fphs-rails/branches/phase3/all/railsapp/fpa-phase3@741 93467e27-a4b6-4d31-bc93-299e2bd7d087

--- fphs-scripts/prep_for_release.sh
+++ fphs-scripts/prep_for_release.sh
@@ -1,8 +1,8 @@
 export FPHS_VERSION=`svn ls --username payres https://open.med.harvard.edu/svn/fphs-rails/tags | sort -V | tail -n 1`
 FPHS_A=`echo $FPHS_VERSION | grep -oP '([0-9]+)' | tail -n 1`
-FPHS_B=2.0.
+FPHS_B=3.0
 #`echo $FPHS_VERSION | grep -oP '([0-9]+).([0-9]+).'`
-export FPHS_VERSION=$FPHS_B$((FPHS_A+1))
+export FPHS_VERSION=$FPHS_B.$((FPHS_A+1))
 echo $FPHS_VERSION
 export DEV_DIR=`pwd`
 cd $DEV_DIR
