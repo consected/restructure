cc968551d180e5f64bf73c425a5d7eec7be75d9e
Phil Ayres <phil_ayres@hms.harvard.edu>
2015-11-25 18:19:50 +0000

    created migration scripts for stage and prod
    git-svn-id: https://open.med.harvard.edu/svn/fphs-rails/branches/phase3/all/railsapp/fpa-phase3@751 93467e27-a4b6-4d31-bc93-299e2bd7d087

--- db/dumps/migrate-nfl-15.dipr.partners.org-1448462409/db-schema-migrations-local.sql
+++ db/dumps/migrate-nfl-15.dipr.partners.org-1448462409/db-schema-migrations-local.sql
@@ -1,3 +1,11 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = UTF8;
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET search_path = ml_app, pg_catalog;
+COPY schema_migrations (version) FROM stdin;
 20150602181200
 20150602181229
 20150602181925
@@ -101,8 +109,6 @@
 20151026181305
 20151028145802
 20151028155426
-20151109223309
-20151120150828
-20151120151912
 20151123203524
 20151124151501
+\.
--- db/dumps/migrate-nfl-16.dipr.partners.org-1448470956/db-schema-migrations-local.sql
+++ db/dumps/migrate-nfl-16.dipr.partners.org-1448470956/db-schema-migrations-local.sql
@@ -1,3 +1,11 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = UTF8;
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET search_path = ml_app, pg_catalog;
+COPY schema_migrations (version) FROM stdin;
 20150602181200
 20150602181229
 20150602181925
@@ -106,3 +114,4 @@
 20151120151912
 20151123203524
 20151124151501
+\.
