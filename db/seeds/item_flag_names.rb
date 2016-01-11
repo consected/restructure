30b732ae1b273e01417c09903988efed189b9e5c
Phil Ayres <phil_ayres@hms.harvard.edu>
2016-01-11 22:06:17 +0000

    ensure tracker updates protocol entries works where it should
    git-svn-id: https://open.med.harvard.edu/svn/fphs-rails/branches/phase3-1@819 93467e27-a4b6-4d31-bc93-299e2bd7d087

--- db/seeds/item_flag_names.rb
+++ db/seeds/item_flag_names.rb
@@ -28,7 +28,7 @@ module Seeds
     def self.setup
       log "In #{self}.setup"
       if Rails.env.test? || ItemFlagName.count == 0
-        if Protocol.count == 0
+        if Protocol.count == 0 || Protocol.where(name: 'Updates').count == 0
           Seeds::TrackerUpdatesProtocol.create_protocol_events
         end
         create_item_flag_names 
