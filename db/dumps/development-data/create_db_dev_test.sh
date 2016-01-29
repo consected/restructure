f7672ccbec610223a0190211d08a5e449477d46f
Phil Ayres <phil_ayres@hms.harvard.edu>
2016-01-29 19:39:03 +0000

    Added a dump of a development database with dummy data, plus a script to quickly create dev and test databases
    git-svn-id: https://open.med.harvard.edu/svn/fphs-rails/branches/phase3-1@945 93467e27-a4b6-4d31-bc93-299e2bd7d087

---
+++ db/dumps/development-data/create_db_dev_test.sh
@@ -0,0 +1,7 @@
+#~/bin/bash
+echo "Enter sudo password to act as postgres to create database"
+
+sudo -u postgres createdb -O `whoami` fpa_development
+sudo -u postgres createdb -O `whoami` fpa_test
+psql -d fpa_development  < ./db/dumps/development-data/data-dump.sql
+psql -d fpa_test  < ./db/dumps/development-data/data-dump.sql
