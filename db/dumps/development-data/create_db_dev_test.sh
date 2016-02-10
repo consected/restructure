059bc03f988bca3614b56fc45701e104530afb85
Phil Ayres <phil_ayres@hms.harvard.edu>
2016-02-10 18:45:38 +0000

    updated development dump
    git-svn-id: https://open.med.harvard.edu/svn/fphs-rails/branches/phase3-1@1050 93467e27-a4b6-4d31-bc93-299e2bd7d087

--- db/dumps/development-data/create_db_dev_test.sh
+++ db/dumps/development-data/create_db_dev_test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #### This script is used by fphs_scripts/setup_dev.sh
-
+#
+# Note: the database dump was created with
+#   sudo -u postgres pg_dump -d fpa_development -O -n ml_app > ./db/dumps/development-data/data-dump.sql
+#
 echo "Enter sudo password to act as postgres to create database"
 
 sudo -u postgres createdb -O `whoami` fpa_development
