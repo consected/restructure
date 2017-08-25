0ac7f4ba849193263a95f38889ad86a5dbb7ba18
Phil Ayres <ayres.phil@gmail.com>
2017-08-25 18:48:19 +0100

    start the activity log ajax processors

--- app/assets/javascripts/_fpa_ajax_processors_activity_logs.js
+++ app/assets/javascripts/_fpa_ajax_processors_activity_logs.js
@@ -1,7 +1,18 @@
 _fpa.postprocessors_activity_logs = {
-  activity_logs_player_contact_result_template: function(block, data){
+  activity_logs_player_contact_phone_type_result_template: function(block, data){
+    _fpa.form_utils.format_block(block);            
     var master_id = data.activity_logs.master_id;
-    $('#activity-logs-generic-' + master_id).addClass('in');
+    $('#activity-logs-' + master_id).addClass('in');
+  },
+  activity_logs_player_contact_email_type_result_template: function(block, data){
+    _fpa.form_utils.format_block(block);            
+    var master_id = data.activity_logs.master_id;
+    $('#activity-logs-' + master_id).addClass('in');
+  },
+  activity_logs_generic_result_template: function(block, data){
+    _fpa.form_utils.format_block(block);            
+    var master_id = data.activity_logs.master_id;
+    $('#activity-logs-' + master_id).addClass('in');
   }
 };
 
