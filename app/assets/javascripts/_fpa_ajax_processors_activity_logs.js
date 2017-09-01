7f34e98fef67d57739b30343acf986a2ee7dd977
philayres <phil.ayres@consected.com>
2017-09-01 16:40:07 +0100

    ajax post processors for activity logs

--- app/assets/javascripts/_fpa_ajax_processors_activity_logs.js
+++ app/assets/javascripts/_fpa_ajax_processors_activity_logs.js
@@ -1,8 +1,9 @@
 _fpa.postprocessors_activity_logs = {
   activity_logs_player_contact_phone_type_result_template: function(block, data){
     _fpa.form_utils.format_block(block);            
-    var master_id = data.activity_logs.master_id;
+    var master_id = data.master_id;
     $('#activity-logs-' + master_id).addClass('in');
+    _fpa.activity_logs.selected_parent(block, {item_id: data.item_id, rec_type: data.rec_type, item_data: data.item_data});
   },
   activity_logs_player_contact_email_type_result_template: function(block, data){
     _fpa.form_utils.format_block(block);            
