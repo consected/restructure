491cc5a22030b6c189d559564fbc05a1315592b9
philayres <phil.ayres@consected.com>
2017-09-08 11:31:13 +0100

    testing of tracker paperclip links

---
+++ spec/features/tracker_record_update_spec.rb
@@ -0,0 +1,135 @@
+require 'rails_helper'
+
+describe "tracker record update", js: true, driver: :app_firefox_driver do
+  
+  include ModelSupport
+  include MasterDataSupport
+  include FeatureSupport
+  
+  before(:all) do
+    
+    create_admin
+    
+    #sp = SubProcess.first
+    
+    ProtocolEvent.enabled.each do |d|
+      d.update! disabled:true, current_admin: @admin#, sub_process: sp
+    end
+    
+    seed_database
+    create_data_set no_trackers: true
+    
+    
+    @user, @good_password  = create_user
+    @good_email  = @user.email
+    
+  end
+
+  before :each do
+    user = User.where(email: @good_email).first
+    expect(user).to be_a User
+    expect(user.id).to equal @user.id
+    
+    #login_as @user, scope: :user
+    
+    login 
+      
+  end
+
+
+
+  def expect_tracker_date_to_be_today
+    sleep 0.01
+    f = find('#tracker_event_date')
+    d = DateTime.now
+    expect(f.value).to match(/0?#{d.month}\/0?#{d.day}\/#{d.year}/)
+  end
+  
+  
+  it "should create a new tracker item when a player record is added or updated" do
+    
+    visit "/masters/search"
+
+    # Switch to advanced search form
+    within '#simple_search_master' do       
+      find('#master_general_infos_attributes_0_first_name').click
+    end
+     within '.advanced-form-selections' do           
+      click_button "Advanced Search"
+    end
+    
+    # Wait for the advance form collapse animation to complete
+    expect(page).to have_css '#master-search-advanced-form.in'    
+    
+    
+    # Search for the player
+    within '#advanced_search_master' do
+      click_link 'clear fields'
+      fill_in "master_player_infos_attributes_0_first_name", with: @full_player_info.first_name
+      fill_in "master_player_infos_attributes_0_last_name", with: "#{@full_player_info.last_name}"
+      find("\#master_player_infos_attributes_0_last_name").send_keys :tab
+    end
+    
+    have_css '#advanced_search_master.ajax-running'
+    expect(page).to have_css "#master_results_block", text: ''
+    expect(page).to have_css "#search_count", text: /[0-9]+/, wait: 10
+    expect(page).not_to have_css '#advanced_search_master.ajax-running'
+    
+    # Check we got some results
+    items = page.all(:css, '.master-expander')
+    expect(items.length).to be > 0
+    
+    # Ensure all the results match what we searched for
+    page.all(:css, '.master-expander .player-info-header .player-names').each do |el|
+      expect(el.text).to match(/#{@full_player_info.first_name.capitalize}.*/)
+    end
+
+    @master_id = all('.master-result .master_id').first.text
+    expect(@master_id).not_to be nil
+    @master = Master.find(@master_id)
+
+
+    # Now jump into the record result    
+    h = open_player_element items.first, items
+    
+
+    # add a player contact phone record
+
+    click_link "Contact record"
+
+    phone = '(615)876-6815'
+    within "form#new_player_contact" do
+      select "Phone", from: "Rec type"
+      fill_in 'Data', with: phone
+      click_button 'Create Player contact'
+    end
+
+    pc_rec = @master.player_contacts.where(data: phone).first
+
+    have_css ".tracker-block.collapse.in"
+
+    protocol = Protocol.active.where(name: 'Updates').first
+
+    paperclip = nil
+
+    # After a moment the tracker will show the newly created item
+    expect(page).to have_css 'tbody[data-template="tracker-result-template"][data-tracker-protocol="'+protocol.name.downcase+'"]'
+    within 'tbody[data-template="tracker-result-template"][data-tracker-protocol="'+protocol.name.downcase+'"]' do |t|
+      have_css('span.record-meta', text: "by #{@user.email}")      
+      paperclip = all("a.tracker-link-to-item[href='#player-contact-#{@master_id}-#{pc_rec.id}']").first
+      expect(paperclip).not_to be nil
+    end
+
+    paperclip.click
+
+
+    expect(page).to have_css("#player-contact-#{@master_id}-#{pc_rec.id}.item-highlight")
+    
+
+  end
+ 
+  after(:all) do
+    
+  end
+end
+
