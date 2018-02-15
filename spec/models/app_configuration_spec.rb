41dde42a3aba47c8e0af9e63c1af0e5c2234294e
Phil Ayres <ayres.phil@gmail.com>
2018-02-15 10:36:19 +0000

    Fixed bad spec

--- spec/models/app_configuration_spec.rb
+++ spec/models/app_configuration_spec.rb
@@ -4,16 +4,23 @@ RSpec.describe AppConfiguration, type: :model do
 
   include ModelSupport
 
-  it "Adds an item to a configuration with blank app type" do
+  it "Adds an item to a configuration with non-blank app type" do
 
     create_admin
-    app_type_id = AppType.active.first.id
+    create_user
+    app_type_id = @user.app_type_id
 
     res = AppConfiguration.create! name: 'some test config', value: 'a value', app_type_id: app_type_id, current_admin: @admin
     expect(res).to be_a AppConfiguration
 
-    res = AppConfiguration.value_for :some_test_config, app_type_id: app_type_id, current_admin: @admin
+    res = AppConfiguration.value_for :some_test_config, @user
     expect(res).to eq 'a value'
+
+    res = AppConfiguration.create! name: 'some test config', value: 'user value', app_type_id: app_type_id, current_admin: @admin, user: @user
+    expect(res).to be_a AppConfiguration
+
+    res = AppConfiguration.value_for :some_test_config, @user
+    expect(res).to eq 'user value'
 
   end
 
