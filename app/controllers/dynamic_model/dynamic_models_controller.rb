0d4233ab25b2eff54f7991812a8d1a289bc3ba60
Phil Ayres <ayres.phil@gmail.com>
2018-02-27 12:10:14 +0000

    Clean up section block dividers (such as external ids) in search results for a cleaner appearance

--- app/controllers/dynamic_model/dynamic_models_controller.rb
+++ app/controllers/dynamic_model/dynamic_models_controller.rb
@@ -1,20 +1,20 @@
 class DynamicModel::DynamicModelsController < ApplicationController
 
-  def edit
-    not_authorized
-  end
-
-  def update
-    not_authorized
-  end
-
-  def new
-    not_authorized
-  end
-
-  def create
-    not_authorized
-  end
+  # def edit
+  #   not_authorized
+  # end
+  #
+  # def update
+  #   not_authorized
+  # end
+  #
+  # def new
+  #   not_authorized
+  # end
+  #
+  # def create
+  #   not_authorized
+  # end
 
   def destroy
     not_authorized
