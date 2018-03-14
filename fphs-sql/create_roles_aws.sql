210c8be4953d61d4a9fafe7bd0f38581b5a3c95f
Phil Ayres <ayres.phil@gmail.com>
2018-03-14 16:15:48 +0000

    Fix assets

--- fphs-sql/create_roles_aws.sql
+++ fphs-sql/create_roles_aws.sql
@@ -15,6 +15,15 @@ BEGIN
    IF NOT EXISTS (
       SELECT *
       FROM   pg_catalog.pg_roles
+      WHERE  rolname = 'fphsrailsapp') THEN
+
+      create user fphsrailsapp role fphsusr;
+      ALTER user fphsrailsapp WITH INHERIT NOCREATEROLE NOCREATEDB LOGIN;
+   END IF;
+
+   IF NOT EXISTS (
+      SELECT *
+      FROM   pg_catalog.pg_roles
       WHERE  rolname = 'fphsadm') THEN
 
       create role fphsadm;
