b4dfb908c1b58f69ff77ed539c27c979c7ef19d7
Phil Ayres <ayres.phil@gmail.com>
2018-03-16 10:59:43 +0000

    SMTP server settings in configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,19 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: ENV['SMTP_SERVER'],
+    port: ENV['SMTP_PORT'],
+    user_name: ENV['SMTP_USER_NAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: (ENV['SMTP_AUTHENTICATION_MODE'] || 'login').to_sym,
+    enable_starttls_auto: true,
+    openssl_verify_mode: :peer,
+    tls: true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
