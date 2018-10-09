30a92ded3529fe7b299e6b02e08f9f62b6f4795d
Phil Ayres <ayres.phil@gmail.com>
2018-10-09 10:23:23 +0100

    Brakeman warning on new unzip. Added whitelist

--- fphs-sql/create-test-db.sh
+++ fphs-sql/create-test-db.sh
@@ -6,7 +6,8 @@ DBOWNER=`whoami`
 cd $(dirname $0)
 
 sudo -u postgres psql -c "create database $DBNAME with owner $DBOWNER;"
-sudo -u postgres psql -d $DBNAME -c "create schema $SCHEMA_NAME;"
-sudo -u postgres psql -d $DBNAME -c "set search_path=$SCHEMA_NAME; create extension if not exists pgcrypto;"
-RAILS_ENV=$APPENV FPHS_POSTGRESQL_DATABASE=$DBNAME rake db:setup
-RAILS_ENV=$APPENV FPHS_POSTGRESQL_DATABASE=$DBNAME rake db:seed
+# sudo -u postgres psql -d $DBNAME -c "create schema $SCHEMA_NAME;"
+# sudo -u postgres psql -d $DBNAME -c "set search_path=$SCHEMA_NAME; create extension if not exists pgcrypto;"
+# RAILS_ENV=$APPENV FPHS_POSTGRESQL_DATABASE=$DBNAME rake db:setup
+# RAILS_ENV=$APPENV FPHS_POSTGRESQL_DATABASE=$DBNAME rake db:seed
+psql -d fpa_test < db/structure.sql
