16c932fa432d8c71afb91ba36be51560a2599973
Phil Ayres <ayres.phil@gmail.com>
2018-11-14 18:51:22 +0000

    Allow readonly access to filestore containers

--- app/controllers/concerns/nfs_store/in_nfs_store_container.rb
+++ app/controllers/concerns/nfs_store/in_nfs_store_container.rb
@@ -29,7 +29,7 @@ module NfsStore
           raise FsException::List.new "activity log type specified is invalid" unless al_class
           @activity_log = al_class.find(alid)
           @activity_log.current_user = current_user
-          raise FsException::NoAccess.new "User does not have access to this activity log" unless @activity_log.allows_current_user_access_to? :edit
+          raise FsException::NoAccess.new "User does not have access to this activity log" unless @activity_log.allows_current_user_access_to? :access
           # Get all the activity logs that may reference this container. Then check the specified one actually does
           refs = ModelReference.find_where_referenced_from(@container).pluck(:from_record_id, :from_record_type)
           in_refs = refs.select {|r| r.first == @activity_log.id && r.last == al_class.to_s}.first
