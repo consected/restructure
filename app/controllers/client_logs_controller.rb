f318a6c8c03b615bcb6faa50cbce911a7cf68497
Phil Ayres <ayres.phil@gmail.com>
2019-05-30 12:15:10 +0100

    Send Javascript errors back to the exception log

---
+++ app/controllers/client_logs_controller.rb
@@ -0,0 +1,23 @@
+class ClientLogsController < ApplicationController
+
+  protect_from_forgery with: :exception, if: Proc.new { |c| c.params[:user_token].blank? }
+  protect_from_forgery with: :null_session, if: Proc.new { |c| !c.params[:user_token].blank? }
+  acts_as_token_authentication_handler_for User
+  before_action :authenticate_user!
+
+  include AppExceptionHandler
+
+  def create
+    log_message params[:msg], params[:error], params[:details]
+  end
+
+  protected
+
+    def log_message msg, error, details
+      user_id = current_user&.id
+      admin_id = current_admin&.id
+      Admin::ExceptionLog.create message: (msg || 'error'), main: error, backtrace: details, user_id: user_id, admin_id: admin_id if Rails.env.production?
+      render text: 'OK', status: 200
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :client_logs, only: [:create]
 
   resources :imports
 
