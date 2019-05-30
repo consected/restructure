b6d722f14b084a701236da4ee08cd49b6ddb7f08
Phil Ayres <ayres.phil@gmail.com>
2019-05-30 12:23:30 +0100

    order results better

--- app/controllers/admin/activity_logs_controller.rb
+++ app/controllers/admin/activity_logs_controller.rb
@@ -1,5 +1,10 @@
 class Admin::ActivityLogsController < AdminController
 
+  protected
+    def default_index_order
+      {updated_at: :desc}
+    end
+
   private
     def permitted_params
       [:name, :item_type, :rec_type, :process_name, :action_when_attribute, :field_list, :blank_log_field_list, :disabled, :blank_log_name, :hide_item_list_panel, :extra_log_types, :main_log_name]
