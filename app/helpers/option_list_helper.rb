52db4193ca0a6fde23dcd6577dea0cc2ed364fa6
Phil <phil.ayres@consected.com>
2019-10-29 18:35:32 +0000

    Refactor app selection for admin

--- app/helpers/option_list_helper.rb
+++ app/helpers/option_list_helper.rb
@@ -4,14 +4,20 @@ module OptionListHelper
     Admin::AppConfiguration.configurations
   end
 
-  def app_type_options
-    options_from_collection_for_select(Admin::AppType.active, 'id', 'label')
+  def app_type_options default_app_type_id: nil
+    if default_app_type_id.is_a? Hash
+      default_app_type_id = default_app_type_id[:selected]
+    end
+
+    options_from_collection_for_select(Admin::AppType.active, 'id', 'label', default_app_type_id)
   end
 
   def app_type_select_current_item use_current_user: false
     if use_current_user
       {selected: current_user&.app_type_id}
-    elsif params[:filter] && params[:filter].first.first == 'app_type_id' && object_instance.app_type_id.blank?
+    elsif object_instance&.app_type_id.present?
+      {selected: object_instance&.app_type_id}
+    elsif params[:filter] && params[:filter].first.first == 'app_type_id' && object_instance&.app_type_id.blank?
       {selected: params[:filter].first.last}
     else
       {}
