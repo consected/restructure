7e007d6c15d4f756fdd9420b838f4a5474d85453
Phil <phil.ayres@consected.com>
2020-01-09 13:40:55 +0000

    Fix mount checking

--- fphs-scripts/setup-dev-filestore.sh
+++ fphs-scripts/setup-dev-filestore.sh
@@ -1,13 +1,4 @@
 #! /bin/bash
-if [ "$(whoami)" == 'root' ]
-then
-  echo Do not run as sudo
-  exit
-else
-  sudo  echo > /dev/null
-fi
-
-
 
 if [ -z "$MOUNTPOINT" ]
 then
@@ -19,27 +10,40 @@ then
   fi
 fi
 
-# if [ $(mountpoint -q $MOUNT_ROOT/gid601) ] && [ $(mountpoint -q $MOUNT_ROOT/gid601) ]
-# then
-#   echo "Setup Dev Filestore"
-# else
-#   echo "Dev Filestore already set up"
-#   exit
-# fi
+FS_ROOT=${MOUNTPOINT}/test-fphsfs
+FS_DIR=main
+MOUNT_ROOT=/mnt/fphsfs
+WEBAPP_USER=${USER}
+
+mountpoint -q $MOUNT_ROOT/gid600
+if [ $? == 0 ]
+then
+  # Already set up. No need to continue.
+
+  echo "mountpoint OK"
+  exit
+fi
+
+if [ "$(whoami)" == 'root' ]
+then
+  echo Do not run as sudo
+  exit
+else
+  sudo  echo > /dev/null
+fi
+
+
 
 if [ "${RAILS_ENV}" != 'test' ]
 then
-  if [ ! "$(mountpoint "${MOUNTPOINT}")" ]
+  mountpoint -q "${MOUNTPOINT}"
+  if [ $? == 1 ]
   then
     echo "${MOUNTPOINT} is not a real mount point. Check the file system is mounted correctly at this location"
     exit 1
   fi
 fi
 
-FS_ROOT=${MOUNTPOINT}/test-fphsfs
-FS_DIR=main
-MOUNT_ROOT=/mnt/fphsfs
-WEBAPP_USER=${USER}
 
 if [ "${RAILS_ENV}" != 'test' ]
 then
@@ -56,7 +60,8 @@ fi
 sudo mountpoint -q $MOUNT_ROOT/gid600 || sudo bindfs --map=@600/@599 --create-for-group=600 --create-for-user=600 --chown-ignore --chmod-ignore --create-with-perms='u=rwD:g=rwD:o=' $FS_ROOT/$FS_DIR $MOUNT_ROOT/gid600
 sudo mountpoint -q $MOUNT_ROOT/gid601 || sudo bindfs --map=@601/@599 --create-for-group=601 --create-for-user=600 --chown-ignore --chmod-ignore --create-with-perms='u=rwD:g=rwD:o=' $FS_ROOT/$FS_DIR $MOUNT_ROOT/gid601
 
-if [ "$(mountpoint -q $MOUNT_ROOT/gid600)" ]
+mountpoint -q $MOUNT_ROOT/gid600
+if [ $? == 1 ]
 then
   echo "Failed to setup mountpoint"
   exit
