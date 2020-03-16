c3aa4a4c979abcd7ce1540ebb346da22b3ca1205
Phil <phil.ayres@consected.com>
2020-03-16 14:30:01 +0000

    Formatting

--- app/models/nfs_store/move_and_rename.rb
+++ app/models/nfs_store/move_and_rename.rb
@@ -1,14 +1,12 @@
+# frozen_string_literal: true
+
 module NfsStore
   class MoveAndRename < MultiActions
-
     self.table_name = 'nfs_store_move_actions'
 
-
-
     def item_actions_field
-      "moved_items"
+      'moved_items'
     end
-
 
     # Move requested files to a new path, returning an array of results
     # Each result is a hash referencing the requested id/retrieval_type
@@ -19,25 +17,24 @@ module NfsStore
     #  file_name is the simple file name
     #  container_path represents the local path within the container
     #  retrieval_path is the filesystem path from which the moved file can be retrieved
-    def move_files selected_items, new_path
-
+    def move_files(selected_items, new_path)
       new_path = Manage::Filesystem.clean_path new_path
 
       setup_items selected_items
 
       if new_path && (new_path.start_with?('.') || new_path.start_with?('/'))
-        raise FsException::Action.new "Path to move to can not start with / or .: #{new_path}"
+        raise FsException::Action, "Path to move to can not start with / or .: #{new_path}"
       end
 
       # Find the depth of the highest path we are planning to move, so we can use this as the root of sub directories to move
       # Otherwise all files, including those in sub directories get dumped directly in the new path
-      all_paths = self.all_action_items.map {|item| item[:retrieved_file].path}.uniq.sort
+      all_paths = all_action_items.map { |item| item[:retrieved_file].path }.uniq.sort
       top_path = all_paths.first
       second_path = all_paths[1]
-      top_paths = all_paths.map {|f| f && f.split('/').compact.first}.uniq
+      top_paths = all_paths.map { |f| f && f.split('/').compact.first }.uniq
 
       if top_paths.length > 1
-        raise FsException::Action.new "Files and folders to move must all be in the same base path. Specified base paths are #{top_paths.join(", ")}"
+        raise FsException::Action, "Files and folders to move must all be in the same base path. Specified base paths are #{top_paths.join(', ')}"
       end
 
       # If we have multiple paths and more than one of the initial ones is at the same depth
@@ -45,22 +42,22 @@ module NfsStore
       # then the top_path is actually a level up
 
       if top_path && second_path && second_path.split('/').length == top_path.split('/').length
-        top_path = top_path.sub(/[^\/]+$/, '').sub(/\/$/, '')
+        top_path = top_path.sub(%r{[^/]+$}, '').sub(%r{/$}, '')
       end
 
-      self.all_action_items.each do |item|
+      all_action_items.each do |item|
         rf = item[:retrieved_file]
         # Replace the top path with the new path, allowing relative moves of sub directories
         # Handle blank top paths carefully
         rf.path = "/#{rf.path}" if top_path.blank?
         rf.path = "#{rf.path}/"
-        new_item_path = rf.path.sub(/^#{top_path}\//, "#{new_path}/").gsub(/(^\/+|\/+$)/, '')
+        new_item_path = rf.path.sub(%r{^#{top_path}/}, "#{new_path}/").gsub(%r{(^/+|/+$)}, '')
         rf.move_to new_item_path
       end
 
       self.new_path = new_path
 
-      self.all_action_items
+      all_action_items
     end
 
     # Rename requested file, returning an array of results
@@ -72,44 +69,42 @@ module NfsStore
     #  file_name is the simple file name
     #  container_path represents the local path within the container
     #  retrieval_path is the filesystem path from which the moved file can be retrieved
-    def rename_file selected_items, new_name
-
-      raise FsException::Action.new "New name is not specified" if new_name.blank?
+    def rename_file(selected_items, new_name)
+      raise FsException::Action, 'New name is not specified' if new_name.blank?
 
       setup_items selected_items
 
-      self.all_action_items.each do |item|
+      all_action_items.each do |item|
         f = item[:retrieved_file]
         archive_file = f.archive_file if f.respond_to? :archive_file
         unless filters_allow_rename?(archive_file, f.path, new_name)
-          raise FsException::Action.new "New name is not allowed. Check it meets the requirements of the 'valid upload files' list"
+          raise FsException::Action, "New name is not allowed. Check it meets the requirements of the 'valid upload files' list"
         end
+
         f.move_to nil, new_name
       end
 
       self.new_path = new_path
 
-      self.all_action_items
+      all_action_items
     end
 
     protected
 
-      def filters_allow_rename? archive_file, path, new_name
-        f = [archive_file, path, new_name].join('/').gsub(/\/\/+/, '/')
-        f = "/#{f}" unless f.start_with? '/'
-        NfsStore::Filter::Filter.evaluate f, activity_log
+    def filters_allow_rename?(archive_file, path, new_name)
+      f = [archive_file, path, new_name].join('/').gsub(%r{//+}, '/')
+      f = "/#{f}" unless f.start_with? '/'
+      NfsStore::Filter::Filter.evaluate f, activity_log
+    end
+
+    def setup_items(selected_items)
+      # Retrieve each file's details. The container_id will be passed if this is a
+      # multi container download, otherwise it will be ignored
+      selected_items.each do |s|
+        container = self.container || Browse.open_container(id: s[:container_id], user: current_user)
+        activity_log = self.activity_log || ActivityLog.open_activity_log(s[:activity_log_type], s[:activity_log_id], current_user)
+        retrieve_file_from(s[:id], s[:retrieval_type], container: container, activity_log: activity_log, for_action: :move_files)
       end
-
-      def setup_items selected_items
-        # Retrieve each file's details. The container_id will be passed if this is a
-        # multi container download, otherwise it will be ignored
-        selected_items.each do |s|
-          container = self.container || Browse.open_container(id: s[:container_id], user: self.current_user)
-          activity_log = self.activity_log || ActivityLog.open_activity_log(s[:activity_log_type], s[:activity_log_id], self.current_user)
-          retrieve_file_from(s[:id], s[:retrieval_type], container: container, activity_log: activity_log, for_action: :move_files)
-        end
-
-      end
-
+    end
   end
 end
