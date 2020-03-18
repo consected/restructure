5409cb71db639c0547e812187fb29b8e2f684983
Phil <phil.ayres@consected.com>
2020-03-18 10:04:10 +0000

    formatting

--- app/models/nfs_store/process/process_handler.rb
+++ app/models/nfs_store/process/process_handler.rb
@@ -1,30 +1,32 @@
+# frozen_string_literal: true
+
 # ProcessHandler provides logic for running a chain of jobs after a file upload
 module NfsStore
   module Process
     class ProcessHandler
-
+      DefaultJobList = %w[mount_archive index_files dicom_metadata].freeze
       attr_accessor :container_file, :parent_item
 
-      def initialize container_file
+      def initialize(container_file)
         self.container_file = container_file
         # Save the parent_item activity log so we can use it to pick up additional configurations
         self.parent_item = container_file.container&.parent_item
       end
 
       def pipeline_config
-        self.parent_item && self.parent_item.extra_log_type_config.nfs_store && self.parent_item.extra_log_type_config.nfs_store[:pipeline]
+        parent_item&.extra_log_type_config&.nfs_store && parent_item.extra_log_type_config.nfs_store[:pipeline]
       end
 
       def pipeline_job_list
-        pipeline_config.map {|p| p.first.first}
+        pipeline_config.map { |p| p.first.first }
       end
 
       # List of valid processing jobs.
       # Can be extended to dynamically select jobs based on container configuration
-      # in the future
       def job_list
         return pipeline_job_list if pipeline_config
-        %w(mount_archive index_files dicom_metadata)
+
+        DefaultJobList
       end
 
       # File path for flag to indicate file is being processed
@@ -43,24 +45,21 @@ module NfsStore
 
       # Run a specific job, based on its name
       # @param name [String] a job type that appears in job_list
-      def run name
+      def run(name)
         return unless name
+
         classname = "#{name}_job".camelize
         c = self.class.parents.first.const_get classname
-        c.perform_later self.container_file
+        c.perform_later container_file
 
         container_file.current_user = container_file.user
         container_file.last_process_name_run = name
         container_file.save!
-
       end
-
-
 
       # Run the next job in the job_list
       # @param current_name [String] name of the current job
-      def run_next_job_after current_name
-
+      def run_next_job_after(current_name)
         next_name = next_job_after current_name
 
         unless next_name
@@ -77,12 +76,12 @@ module NfsStore
       # Find the name of the next job in the job_list
       # @param name [String] name of the previous job
       # @return [String] name of the next job in the job_list
-      def next_job_after name
+      def next_job_after(name)
         i = job_list.index(name)
         return unless i
-        job_list[i+1]
-      end
 
+        job_list[i + 1]
+      end
     end
   end
 end
