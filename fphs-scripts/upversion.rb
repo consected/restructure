49621217a598552b9cca9f3a06d6399a3a1cd3b7
Phil <phil.ayres@consected.com>
2020-04-06 15:38:20 +0100

    Upversion updates

--- fphs-scripts/upversion.rb
+++ fphs-scripts/upversion.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
-VERSION_FILE = 'version.txt'
+# frozen_string_literal: true
+
+VERSION_FILE = ENV['VERSION_FILE'] || 'version.txt'
 v = File.read(VERSION_FILE)
 vs = v.strip.split('.')
 vs[2] = vs[2].to_i + 1
 newver = vs.join('.')
-File.open(VERSION_FILE, 'w') { |f|  f.write newver }
+File.open(VERSION_FILE, 'w') { |f| f.write newver } unless ARGV.include?('-p')
 puts newver
