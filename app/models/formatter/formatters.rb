f1db38b78aa5610b2b0456e8673927a05b19c248
Phil <phil.ayres@consected.com>
2020-05-21 17:02:36 +0100

    formatting

--- app/models/formatter/formatters.rb
+++ app/models/formatter/formatters.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
+
 module Formatter
   module Formatters
-
     extend ActiveSupport::Concern
 
     class_methods do
-
-      def formatter_for type
+      def formatter_for(type)
         return unless type
+
         "Formatter::#{type.to_s.split('::').last.classify}".safe_constantize
       end
 
-      def formatter_do type, data, options=nil
+      def formatter_do(type, data, options = nil)
         options ||= {}
         ff = formatter_for(type)
         if ff
@@ -20,7 +21,7 @@ module Formatter
         end
       end
 
-      def formatter_error_message type, data
+      def formatter_error_message(type, data)
         ff = formatter_for(type)
         if ff
           ff.format_error_message(data)
@@ -29,13 +30,11 @@ module Formatter
         end
       end
 
-      def format_data_attribute attr_conf, obj
+      def format_data_attribute(attr_conf, obj)
         attr_conf = [attr_conf] if attr_conf.is_a? String
-        res = attr_conf.map {|i| a = obj.attributes[i]; obj.attribute_names.include?(i) ? formatter_do(a.class, a, current_user: obj.current_user) : i }
-        return res.join(' ')
+        res = attr_conf.map { |i| a = obj.attributes[i]; obj.attribute_names.include?(i) ? formatter_do(a.class, a, current_user: obj.current_user) : i }
+        res.join(' ')
       end
-
     end
-
   end
 end
