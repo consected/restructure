54b8e6a9d44b5b35dce89de73d98cbe986f80c76
Phil <phil.ayres@consected.com>
2020-05-28 12:49:49 +0100

    seed naming errors

--- db/seeds/0_c_masters.rb
+++ db/seeds/0_c_masters.rb
@@ -1,21 +1,21 @@
+# frozen_string_literal: true
+
 require_dependency 'master'
 module Seeds
   module CMasters
-
     def self.do_first
       true
     end
 
     def self.setup
       log "In #{self}.setup"
-
-      unless Master.where(id: -1).first
+      BUsers.setup
+      if Master.where(id: -1).first
+        log "Did not run #{self}.setup"
+      else
         Master.create! id: -1, current_user: User.active.first
         log "Ran #{self}.setup"
-      else
-        log "Did not run #{self}.setup"
       end
-
     end
   end
 end
