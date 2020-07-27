c4b46a2728a3c7e459b0aef2ad71da0aaa49995d
Phil <phil.ayres@consected.com>
2020-07-27 17:47:16 +0100

    Formatting

--- app/models/concerns/alternative_ids.rb
+++ app/models/concerns/alternative_ids.rb
@@ -1,5 +1,6 @@
-module AlternativeIds
+# frozen_string_literal: true
 
+module AlternativeIds
   extend ActiveSupport::Concern
 
   included do
@@ -7,15 +8,15 @@ module AlternativeIds
   end
 
   class_methods do
-
     def crosswalk_attrs
-      self.attribute_names.map(&:to_sym) - [:id, :master_id, :user_id, :created_at, :updated_at, :rank]
+      attribute_names.map(&:to_sym) - %i[id master_id user_id created_at updated_at rank]
     end
 
     def external_id_matching_fields
       # Cache the result, because it speeds up template use of ids hugely
       return @stored_external_id_matching_fields if @stored_external_id_matching_fields
-      @stored_external_id_matching_fields = ExternalIdentifier.active_model_configurations.map{|f| f.external_id_attribute.to_sym}
+
+      @stored_external_id_matching_fields = ExternalIdentifier.active_model_configurations.map { |f| f.external_id_attribute.to_sym }
     end
 
     # Force a reset of the external fields, allowing new definitions to appear
@@ -23,7 +24,7 @@ module AlternativeIds
       @stored_external_id_matching_fields = nil
     end
 
-    def external_id? attr_name
+    def external_id?(attr_name)
       external_id_matching_fields.include? attr_name
     end
 
@@ -31,58 +32,55 @@ module AlternativeIds
       crosswalk_attrs + external_id_matching_fields
     end
 
-    def external_id_definition attr_name
+    def external_id_definition(attr_name)
       ExternalIdentifier.active.where(external_id_attribute: attr_name).first
     end
 
-    def find_with_alternative_id field_name, value
+    def find_with_alternative_id(field_name, value)
       return if value.blank?
+
       field_name = field_name.to_sym
       # Start by attempting to match on a field in the master record
-      raise "Can not match on this field. It is not an accepted alterative ID field. #{field_name}" unless alternative_id_fields.include?(field_name)
-      return self.where(field_name => value).first if self.attribute_names.include?(field_name.to_s)
+      unless alternative_id_fields.include?(field_name)
+        raise "Can not match on this field. It is not an accepted alterative ID field. #{field_name}"
+      end
+      return where(field_name => value).first if attribute_names.include?(field_name.to_s)
 
       # No master record field was found. So try an external ID instead
       if external_id_matching_fields.include?(field_name.to_sym)
         ei = ExternalIdentifier.class_for(field_name).find_by_external_id(value)
-        if ei
-          return ei.master
-        else
-          return nil
-        end
+        ei&.master
       else
-        raise "The field specified is not valid for external identifier matching"
+        raise 'The field specified is not valid for external identifier matching'
       end
-
     end
-
-
   end
 
-
-  def alternative_id_value field_name
+  def alternative_id_value(field_name)
     field_name = field_name.to_sym
     # Start by attempting to match on a field in the master record
-    raise "Can not match on this field. It is not an accepted alterative ID field. #{field_name}" unless self.class.alternative_id_fields.include?(field_name)
+    unless self.class.alternative_id_fields.include?(field_name)
+      raise "Can not match on this field. It is not an accepted alterative ID field. #{field_name}"
+    end
 
-    return self.attributes[field_name.to_s] if self.class.crosswalk_attrs.include?(field_name)
+    return attributes[field_name.to_s] if self.class.crosswalk_attrs.include?(field_name)
 
     eid = self.class.external_id_definition(field_name)
-    raise FphsException.new "External ID definition is not active for #{field_name}" unless eid
+    raise FphsException, "External ID definition is not active for #{field_name}" unless eid
+
     assoc_name = eid.model_association_name
 
     if self.class.external_id_matching_fields.include?(field_name.to_sym)
-      m = self.send(assoc_name).first
+      m = send(assoc_name).first
       return unless m
+
       m.external_id
     end
-
   end
 
   def alternative_ids
     res = {}
-    self.class.alternative_id_fields.each {|f|  res[f] = self.alternative_id_value(f) }
+    self.class.alternative_id_fields.each { |f| res[f] = alternative_id_value(f) }
     res
   end
-
 end
