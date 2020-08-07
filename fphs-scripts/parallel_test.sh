6b1aa579fcf0495cb8221f652178c0fc7dd18e8a
Phil <phil.ayres@consected.com>
2020-08-07 15:47:08 +0100

    script cleanup

--- fphs-scripts/parallel_test.sh
+++ fphs-scripts/parallel_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # First, run brakeman
-brakeman -q --summary >/tmp/fphs-brakeman-summary.txt
+bin/brakeman -q --summary > /tmp/fphs-brakeman-summary.txt
 cat /tmp/fphs-brakeman-summary.txt
-echo >log/test.log
+echo > log/test.log
 # Run the rspec tests in parallel. Use the first arg to define the path if needed
 export PARALLEL_TEST_PROCESSORS=8
 bundle exec rake parallel:spec[$1]
