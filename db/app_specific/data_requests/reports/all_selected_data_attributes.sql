65c05da4fb2d95beb9b839340133d0770dac861d
Phil <phil.ayres@consected.com>
2020-09-10 18:28:42 +0100

    Data requests app updates

--- db/app_specific/data_requests/reports/all_selected_data_attributes.sql
+++ db/app_specific/data_requests/reports/all_selected_data_attributes.sql
@@ -3,6 +3,19 @@
 with datadics as (
   select distinct
     id,
+    'Q1' "study",
+    variable_name,
+    domain,
+    field_label,
+    field_attributes,
+    field_note,
+    'q1_datadic' "datadic" 
+  from q1_datadic
+  
+  union
+  
+  select distinct
+    id,
     'Q2' "study",
     variable_name,
     domain,
@@ -42,7 +55,21 @@ on
   sel.record_id = cb.id 
   and not coalesce(sel.disabled, false) 
   and cb.datadic = sel.record_type
-  and sel.data_request_id = :list_id 
+  and (
+    :list_type is NULL 
+    and sel.data_request_id = :list_id 
+   
+    OR 
+    :list_type = 'ActivityLog::DataRequestAssignment'
+    and sel.data_request_id = (
+      select to_record_id from model_references mr
+      where from_record_type = :list_type
+      and from_record_id = :list_id
+      and to_record_type = 'DynamicModel::DataRequest'
+      order by id desc
+      limit 1
+    )
+  )
 
 where
   sel.id IS NOT NULL
