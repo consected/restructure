a7c1956a427ff8708c645116bf83b866f7e37e6a
Phil <phil.ayres@consected.com>
2020-09-21 21:29:50 +0100

    ipa enhancements

--- fphs-sql/grant_roles_access_to_ipa_ops.sql
+++ fphs-sql/grant_roles_access_to_ipa_ops.sql
@@ -1,35 +1,61 @@
 REVOKE ALL ON SCHEMA ipa_ops FROM fphs;
+
 GRANT ALL ON SCHEMA ipa_ops TO fphs;
+
 GRANT USAGE ON SCHEMA ipa_ops TO fphsadm;
+
 GRANT USAGE ON SCHEMA ipa_ops TO fphsusr;
+
 GRANT USAGE ON SCHEMA ipa_ops TO fphsetl;
 
-
 GRANT ALL ON ALL TABLES IN SCHEMA ipa_ops TO fphs;
-GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA ipa_ops TO fphsusr;
-GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA ipa_ops TO fphsetl;
-GRANT SELECT,INSERT,DELETE,TRUNCATE,UPDATE ON ALL TABLES IN SCHEMA ipa_ops TO fphsadm;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA ipa_ops TO fphsusr;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA ipa_ops TO fphsetl;
+
+GRANT SELECT, INSERT, DELETE, TRUNCATE, UPDATE ON ALL TABLES IN SCHEMA ipa_ops TO fphsadm;
 
 GRANT ALL ON ALL SEQUENCES IN SCHEMA ipa_ops TO fphs;
-GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA ipa_ops TO fphsusr;
-GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA ipa_ops TO fphsetl;
-GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA ipa_ops TO fphsadm;
 
+GRANT SELECT, USAGE ON ALL SEQUENCES IN SCHEMA ipa_ops TO fphsusr;
 
-DO
-$body$
+GRANT SELECT, USAGE ON ALL SEQUENCES IN SCHEMA ipa_ops TO fphsetl;
+
+GRANT SELECT, USAGE ON ALL SEQUENCES IN SCHEMA ipa_ops TO fphsadm;
+
+DO $body$
 BEGIN
+  IF EXISTS (
+    SELECT
+      *
+    FROM
+      pg_catalog.pg_roles
+    WHERE
+      rolname = 'fphsrailsapp') THEN
+  GRANT USAGE ON SCHEMA ipa_ops TO fphsrailsapp;
 
-IF EXISTS (
-   SELECT *
-   FROM   pg_catalog.pg_roles
-   WHERE  rolname = 'fphsrailsapp') THEN
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA ipa_ops TO fphsrailsapp;
 
-   GRANT USAGE ON SCHEMA ipa_ops TO fphsrailsapp;
-   GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA ipa_ops TO fphsrailsapp;
-   GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA ipa_ops TO fphsrailsapp;
+GRANT SELECT, USAGE ON ALL SEQUENCES IN SCHEMA ipa_ops TO fphsrailsapp;
+
 END IF;
 
+END $body$; DO $body$
+BEGIN
+  IF EXISTS (
+    SELECT
+      *
+    FROM
+      pg_catalog.pg_roles
+    WHERE
+      rolname = 'fphsrailsapp1') THEN
+  GRANT USAGE ON SCHEMA ipa_ops TO fphsrailsapp1;
 
-END
-$body$;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA ipa_ops TO fphsrailsapp1;
+
+GRANT SELECT, USAGE ON ALL SEQUENCES IN SCHEMA ipa_ops TO fphsrailsapp1;
+
+END IF;
+
+END $body$;
