b9400f6f0377a681e6d7327260e13291eb5744b1
Phil <phil.ayres@consected.com>
2020-11-03 15:29:15 +0000

    Tracker bugfix

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
+# A tracker may be created or viewed similar to any other instance type related to a master record.
+# Updates are handled differently, since they are effectively merged in an existing top level
+# Tracker instance if there is one, alternatively a new Tracker record is created.
+# Underlying database triggers supporting the model handle this 'upsert'.
 class TrackersController < UserBaseController
   include MasterHandler
 
@@ -10,7 +16,6 @@ class TrackersController < UserBaseController
   # This ensures that the DB can make the decision which tracker history entry actually
   # appears in the trackers table as the latest item for a specific protocol.
   def update
-
     # Start by building a new tracker item that reflects the existing item that
     # Rails believes is being updated
     new_tracker = @master_objects.build
@@ -28,7 +33,7 @@ class TrackersController < UserBaseController
     # Guard against foreign key constraint errors
     begin
       res = new_tracker.update(secure_params)
-    rescue => e
+    rescue StandardError => e
       logger.warn "Tracker update error: #{e.inspect}"
       new_tracker.errors.add :protocol_id, "Tracker update error: #{e.inspect}"
     end
@@ -39,7 +44,7 @@ class TrackersController < UserBaseController
       # Guard against foreign key constraint errors
       begin
         res = new_tracker.merge_if_exists
-      rescue => e
+      rescue StandardError => e
         logger.warn "Tracker update merge error: #{e.inspect}"
 
         @tracker.errors.add :protocol_id, "Tracker update error: #{e.inspect}"
@@ -56,56 +61,61 @@ class TrackersController < UserBaseController
       end
     else
       # Save the error from the failed update to the @tracker to return to the end user
-      logger.info "Error from tracker: #{new_tracker.errors.first.first}"
-      object_instance.errors.add new_tracker.errors.first.first
+      logger.info "Error from tracker: #{new_tracker.errors}"
+      object_instance.errors.add :tracker, new_tracker.errors.first.join(' ')
     end
 
     logger.warn "Error updating #{human_name}: #{object_instance.errors.inspect}"
     render json: object_instance.errors, status: :unprocessable_entity
-
   end
-
 
   private
 
-    def set_additional_attributes obj
+  #
+  # Set additional attributes from params
+  # Specifically we set up @item instance based on the record_type and record_id params.
+  # @param [Tracker] obj - the current tracker instance
+  # @return [true]
+  def set_additional_attributes obj
+    return true if params[:record_type].blank? || params[:record_id].blank?
 
-      return true if params[:record_type].blank? || params[:record_id].blank?
-      item_class_name = params[:record_type].singularize.camelize
+    item_class_name = params[:record_type].singularize.camelize
 
-      # Find the matching UserBase subclass that has this name, avoiding using the supplied param
-      # in a way that could be risky by allowing code injection
-      ic = UserBase.class_from_name item_class_name
+    # Find the matching UserBase subclass that has this name, avoiding using the supplied param
+    # in a way that could be risky by allowing code injection
+    ic = UserBase.class_from_name item_class_name
 
-      # look up the item using the item_id parameter.
-      rid = params[:record_id].to_i
-      @item  = ic.find(rid)
+    # look up the item using the item_id parameter.
+    rid = params[:record_id].to_i
+    @item = ic.find(rid)
 
-      if @item
-        obj.item_id = @item.id
-        obj.item_type = @item.class.name
-      end
-      true
+    if @item
+      obj.item_id = @item.id
+      obj.item_type = @item.class.name
     end
+    true
+  end
 
+  #
+  # Merge the provided data into an existing protocol if it exists in this master
+  # @return [nil|false] return nil if the tracker was not merged or false otherwise
+  def merge_entry_if_exists
+    @tracker = @master_objects.build(secure_params)
+    set_additional_attributes @tracker
+    res = @tracker.merge_if_exists
+    # If the tracker record exists with the requested protocol then return the updated record and show the result
+    # Otherwise just follow the default record creation flow
+    return unless res
 
-    def merge_entry_if_exists
-      @tracker = @master_objects.build(secure_params)
-      set_additional_attributes @tracker
-      logger.info "===>#{@tracker.inspect}"
-      res = @tracker.merge_if_exists
-      logger.info "==+++=>#{res.inspect}"
-      # If the tracker record exists with the requested protocol then return the updated record and show the result
-      # Otherwise just follow the default record creation flow
-      if res
-        @tracker = res
-        show
-        # prevent additional renders by returning false
-        return false
-      end
-    end
+    @tracker = res
+    show
+    # prevent additional renders by returning false
+    false
+  end
 
-    def secure_params
-      params.require(:tracker).permit(:master_id,  :protocol_id, :protocol_event_id, :event_date, :sub_process_id, :user_id, :notes, :item_id, :item_type)
-    end
+  def secure_params
+    params.require(:tracker).
+      permit(:master_id, :protocol_id, :sub_process_id, :protocol_event_id, :event_date,
+             :user_id, :notes, :item_id, :item_type)
+  end
 end
