a32b0f79ba6bff3526624970cdb86a290d3f8b71
Phil <phil.ayres@consected.com>
2021-01-06 10:22:48 +0000

    Bugfixes (from ReStructure project)

--- app-scripts/create-demo-db.sh
+++ app-scripts/create-demo-db.sh
@@ -6,7 +6,7 @@ cd $(dirname $0)
 
 sudo -u postgres psql -c "create database $DBNAME with owner $DBOWNER;"
 sudo -u postgres psql -d $DBNAME -c "create schema $SCHEMA_NAME;"
-sudo -u postgres psql -d $DBNAME < create_roles.sql
+sudo -u postgres psql -d $DBNAME < create-roles.sql
 sudo -u postgres psql -d $DBNAME < grant_roles_access_to_ml_app.sql
 sudo -u postgres psql -d $DBNAME -c "alter user fphsetl login; alter user fphsetl  password 'fphs';"
 sudo -u postgres psql -d $DBNAME -c "GRANT ALL ON SCHEMA $SCHEMA_NAME TO $DBOWNER;"
--- db/seeds/activity_log_player_contact_phone.rb
+++ db/seeds/activity_log_player_contact_phone.rb
@@ -10,10 +10,12 @@ module Seeds
     end
 
     def self.create_phone_log_admin_activity_log
-      res = ActivityLog.find_or_initialize_by(name: 'Phone Log', item_type: 'player_contact', rec_type: 'phone', disabled: false, action_when_attribute: 'called_when',
-                                              field_list: 'data, select_call_direction, select_who, called_when, select_result, select_next_step, follow_up_when, notes, protocol_id, set_related_player_contact_rank',
-                                              blank_log_field_list: 'select_who, called_when, select_next_step, follow_up_when, notes, protocol_id')
-
+      res = ActivityLog.where(name: 'Phone Log').first
+      unless res
+        res = ActivityLog.find_or_initialize_by(name: 'Phone Log', item_type: 'player_contact', rec_type: 'phone', disabled: false, action_when_attribute: 'called_when',
+                                                field_list: 'data, select_call_direction, select_who, called_when, select_result, select_next_step, follow_up_when, notes, protocol_id, set_related_player_contact_rank',
+                                                blank_log_field_list: 'select_who, called_when, select_next_step, follow_up_when, notes, protocol_id')
+      end
       unless res.active_model_configuration?
         Trackers.setup
         GeneralSelections.setup
