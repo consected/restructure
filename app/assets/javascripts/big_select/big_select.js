777c2675084e7240278dcf11fb9a5df3d366574e
Phil <phil.ayres@consected.com>
2021-03-24 13:03:33 +0000

    Add grouping to big-select

--- app/assets/javascripts/big_select/big_select.js
+++ app/assets/javascripts/big_select/big_select.js
@@ -1,41 +1,111 @@
-$.big_select = function ($field, $target, hash, before, after) {
+$.big_select = function ($field, $target, full_hash, before, after) {
 
-  var setup_with = function (hash, $target, value) {
-    var res = $target.append(`<div class="big-select"></div>`);
+  var setup_with = function (hash, $target, selected_value) {
+    var $outer = $target.append(`<div class="big-select"></div>`);
+    var group_num = 0
     for (var k of Object.keys(hash)) {
       var val = hash[k]
-      var selected_class = (value == k) ? 'bsi-selected' : ''
-      var html = `
-    <div class="big-select-item ${selected_class}" data-bsi-key="${k}">
+
+      if (typeof val == 'object') {
+        var $group_title_h = $(`
+        <div class="big-select-group-head" data-bsi-key="${k}" id="big-select-group-head--${field_parent_id}-${group_num}">
+          <a role="button" data-toggle="collapse" data-parent="#${field_parent_id}" class="collapsed"
+             href="#big-select-panel--${field_parent_id}-${group_num}" aria-expanded="true" aria-controls="big-select-item--${k}">
+             <i class="caret"></i> ${k}
+          </a>
+        </div>`
+        )
+        $outer.append($group_title_h)
+
+        var $panel_h = $(`
+        <div class="collapse" role="tabpanel" aria-labelledby="big-select-group-head--${field_parent_id}-${group_num}" 
+        id="big-select-panel--${field_parent_id}-${group_num}"></div>
+        `)
+
+        $outer.append($panel_h)
+
+        for (var k_in of Object.keys(val)) {
+          var val_in = val[k_in]
+          append_item(k_in, val_in, selected_value, $panel_h)
+        }
+
+        group_num++
+      }
+      else {
+        append_item(k, val, selected_value, $outer)
+      }
+
+    }
+
+    return $outer
+  }
+
+  var append_item = function (k, val, selected_value, $container) {
+    var selected_class = (selected_value == k) ? 'bsi-selected' : ''
+    var html = `
+    <div class="big-select-item ${selected_class}" data-bsi-key="${k}" id="big-select-item--${k}">
       <div class="bsi--head">${k}</div>
       <div class="bsi--body">${val}</div>
     </div>
     `
-      res.append(html)
-    }
-
-    return res
+    $container.append(html)
   }
 
   var set_info = function (init_val) {
     if (!init_val) return
+
+    var flat_hash = flatten_hash(hash)
+    var val = flat_hash[init_val]
     $desc.attr('title', 'show definition')
-    $desc.attr('data-content', hash[init_val]);
+    $desc.attr('data-content', val);
   }
 
+
+  // Hashes can be simple key : value pairs, or they 
+  // can be group : key : value, where the group is essentially disposed of
+  var flatten_hash = function (hash) {
+    var fh = {}
+
+    for (var k of Object.keys(hash)) {
+      var val = hash[k]
+      if (typeof val == 'object') {
+        for (var k_in of Object.keys(val)) {
+          fh[k_in] = val[k_in]
+        }
+      }
+      else {
+        fh[k] = val
+      }
+    }
+
+    return fh
+  }
+
+  var set_hash = function (full_hash) {
+    var subtype = $field.attr('data-big-select-subtype') || 'big_select_default'
+    return full_hash[subtype]
+  }
+
+  //
+  // Start initialization
+  //
   var res
   var init_val = $field.val()
 
   var $field_parent = $field.parent()
+  var field_parent_id = $field_parent.prop('id')
   var $desc = $field_parent.find('.big-select-description')
 
   $desc.popover({ trigger: 'click hover' });
+
+  var hash = set_hash(full_hash)
   set_info(init_val)
-
-
 
   $field.on('focus', function () {
     if (before) before()
+    $desc.popover('hide')
+    hash = set_hash(full_hash)
+    if (!hash) return
 
     res = setup_with(hash, $target, $field.val());
 
--- app/helpers/big_select_field_helper.rb
+++ app/helpers/big_select_field_helper.rb
@@ -6,19 +6,37 @@ module BigSelectFieldHelper
   # @param [Form] form
   # @param [Hash] data - key value definitions
   # @return [String] - html result
-  def big_select_field(form, field, data)
-    field_html = form.text_field field, class: 'use-big-select', readonly: 'readonly'
-    <<~END_SQL
-      <span class="big-select-wrapper">
+  def big_select_field(form, field, data, subtype: nil)
+    field_id = "#{form.object_name}_#{field}"
+
+    field_html = form.text_field field,
+                                 class: 'use-big-select',
+                                 readonly: 'readonly',
+                                 id: field_id,
+                                 data: { 'big-select-subtype': subtype }
+
+    <<~END_HTML
+      <span class="big-select-wrapper" id="bsw-#{field_id}" data-big-select-field-id="#{field_id}">
         #{field_html}
         <span class="glyphicon glyphicon-info-sign big-select-description"
           data-toggle="popover"
           title="" data-content=""></span>
-        <script>
-          $('#admin_app_configuration_name')[0].big_select_hash = #{data.to_json.html_safe};
-        </script>
+        #{big_select_field_data(field_id, subtype, data)}
       </span>
-    END_SQL
+    END_HTML
+      .html_safe
+  end
+
+  def big_select_field_data(field_id, subtype, data)
+    subtype ||= 'big_select_default'
+
+    <<~END_HTML
+      <script>
+        var big_select_field = $('##{field_id}')[0]
+        big_select_field.big_select_hash = big_select_field.big_select_hash || {}
+        big_select_field.big_select_hash['#{subtype}'] = #{data.to_json.html_safe};
+      </script>
+    END_HTML
       .html_safe
   end
 end
