a550bd5e5b3e33ba576643effdef195c61dc8813
Phil <phil.ayres@consected.com>
2021-05-20 11:26:07 +0100

    Update restarter to work on Amazon Linux 2 with systemd service

--- app-scripts/restart_delayed_job.sh
+++ app-scripts/restart_delayed_job.sh
@@ -7,44 +7,8 @@ if [ ! -f '/opt/elasticbeanstalk/bin/get-config' ]; then
   exit
 fi
 
-if [ -z "${NUM_WORKERS}" ] || [ "${NUM_WORKERS}" == '0' ]; then
-  echo "No workers requested"
-  exit
-fi
-
-EB_SCRIPT_DIR=/opt/elasticbeanstalk/support/scripts
-EB_SUPPORT_DIR=/opt/elasticbeanstalk/support
-EB_APP_USER=webapp
-EB_APP_CURRENT_DIR=/var/app/current
-EB_APP_PIDS_DIR=/var/app/support/pids
-
-if [ $(whoami) != "${EB_APP_USER}" ]; then
-  echo "Must be run as ${EB_APP_USER}"
-  exit 3
-fi
-
-# Setting up correct environment and ruby version so that bundle can load all gems
-echo $EB_SUPPORT_DIR/envvars
-. $EB_SUPPORT_DIR/envvars
-. $EB_SCRIPT_DIR/use-app-ruby.sh
-
-cd $EB_APP_CURRENT_DIR
-
-source /etc/profile
-# Provide a little time for delayed_job calling this to finish the job
-sleep 2
-bundle exec bin/delayed_job -n $NUM_WORKERS --pid-dir=$EB_APP_PIDS_DIR stop
-
-sleeptime=20
-for p in $(pgrep -u webapp -f delayed_job); do
-  sleep $sleeptime
-  sleeptime=0
-  if [ $p != $$ ]; then
-    kill $p 2> /dev/null
-  fi
-done
-sleep 2
-
-bundle exec bin/delayed_job -n $NUM_WORKERS --pid-dir=$EB_APP_PIDS_DIR start
+# This relies on delayed_job being set up as a systemd service, which will be restarted automatically.
+# The AWS Elastic Beanstalk *Procfile* defines this service.
+pkill --signal 15 -f bin/delayed_job
 
 echo 'Done'
