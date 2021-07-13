a83c8d61f8ed82286fa39a1c11ca13d348e6726d
Phil <phil.ayres@consected.com>
2021-07-13 20:49:47 +0100

    Fix restarter

--- app-scripts/restart_app_server.sh
+++ app-scripts/restart_app_server.sh
@@ -9,6 +9,14 @@ fi
 
 # This relies on the app server being set up as a systemd service, which will be restarted automatically.
 # The AWS Elastic Beanstalk *Procfile* defines this service as 'web'.
-kill --signal 15 -f $PPID
+# Also, the user running the application server needs to be in the sudoers file, to allow the service
+# to be restarted. Set it up as root, with:
+
+# cat > /etc/sudoers.d/webapp << SUDO
+# webapp ALL= NOPASSWD: /bin/systemctl restart web.service
+# webapp ALL= NOPASSWD: /bin/systemctl restart delayed_job.service
+# SUDO
+
+sudo /bin/systemctl restart web.service
 
 echo 'Done'
