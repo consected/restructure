53d4c5f54da6109e430f92d9fd66db35cb1cfc41
Phil <phil.ayres@consected.com>
2021-07-20 20:22:48 +0100

    Handle split zips and test broken zips

--- app-scripts/extract_archive.sh
+++ app-scripts/extract_archive.sh
@@ -9,11 +9,25 @@ if [ -z "${archive_path}" ] || [ -z "${tmpzipdir}" ]; then
   exit 3
 fi
 
+single_zip_path=$(mktemp /tmp/nfs-store-unzip-single-XXXXXXX)
 tmpres=$(mktemp /tmp/nfs-store-unzip-res-XXXXXXX)
-unzip -n "${archive_path}" -d "${tmpzipdir}" > "${tmpres}"
+
+rm -f ${single_zip_path}
+
+# Join split files to a single file in the temp directory
+zip -q -s 0 "${archive_path}" -O "${single_zip_path}"
+if [ $? != 0 ]; then
+  rm -f "${single_zip_path}"
+  echo >&2 "Failed zip -q -s 0 '${archive_path}' -O '${single_zip_path}'"
+  exit 7
+fi
+
+unzip -n "${single_zip_path}" -d "${tmpzipdir}" > "${tmpres}"
 
 if [ $? != 0 ]; then
   rm -f "${tempres}"
+  rm -f "${single_zip_path}"
+
   echo >&2 "Failed unzip -n '${archive_path}' -d '${tmpzipdir}'"
   exit 1
 fi
@@ -21,6 +35,7 @@ fi
 resnum=$(grep -E '(inflating|extracting):' "${tmpres}" | wc -l)
 filecount=$(find "${tmpzipdir}" -type f | wc -l)
 rm -f "${tempres}"
+rm -f "${single_zip_path}"
 
 if [ "${resnum}" != "${filecount}" ]; then
   echo >&2 "${resnum}" != "${filecount}"
