5e11f080376a3e2696936a41de42d8be0b387f4e
Phil <phil.ayres@consected.com>
2021-08-19 19:36:20 +0100

    Add inflection acronyms

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -3,14 +3,17 @@
 # Add new inflection rules using the following format. Inflections
 # are locale specific, and you may define rules for as many different
 # locales as you wish. All of these examples are active by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  # Allow status to be pluralized to statuses not stati
+end
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
 
+acronymns = %w[IPA PI BHS]
 # These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym 'RESTful'
-# end
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  acronymns.each { |a| inflect.acronym a }
+end
