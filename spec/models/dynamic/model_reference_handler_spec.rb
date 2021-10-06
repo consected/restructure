14dcac3ad47ce7d161f3f749e088eca9acba1701
Phil <phil.ayres@consected.com>
2021-10-06 21:44:05 +0100

    Transfer from Harvard @7.4.62

--- spec/models/dynamic/model_reference_handler_spec.rb
+++ spec/models/dynamic/model_reference_handler_spec.rb
@@ -70,6 +70,24 @@ RSpec.describe 'Activity Log extra types implementation', type: :model do
                 reference:
                   data: '#{@working_data}'
 
+
+      mr_self_ref:
+        label: Reference Self Test
+        fields:
+          - select_call_direction
+          - select_who
+          - disabled
+        editable_if:
+          always: true
+        references:
+          player_contacts:
+            from: master
+            add: many
+          activity_log__player_contact_elt:
+            from: this
+            add: many
+            also_disable_record: true
+
     END_DEF
 
     al.current_admin = @admin
@@ -90,9 +108,14 @@ RSpec.describe 'Activity Log extra types implementation', type: :model do
     expect(c4.label).to eq 'Reference Showable Test'
     expect(c4.fields).to eq %w[select_call_direction select_who]
 
+    c5 = al.option_configs[4]
+    expect(c5.label).to eq 'Reference Self Test'
+    expect(c5.fields).to eq %w[select_call_direction select_who disabled]
+
     setup_access al.resource_name, resource_type: :table, user: @user
     setup_access c3.resource_name, resource_type: :activity_log_type, user: @user
     setup_access c4.resource_name, resource_type: :activity_log_type, user: @user
+    setup_access c5.resource_name, resource_type: :activity_log_type, user: @user
   end
 
   it 'saves data into an activity log record' do
@@ -146,5 +169,51 @@ RSpec.describe 'Activity Log extra types implementation', type: :model do
     al_showable.reset_model_references
     expect(al_showable.model_references.length).to eq 0
     expect(al_simple.model_references.length).to be > 0
+  end
+
+  it 'handles disabling of references and referenced items' do
+    puts @activity_log.resource_name
+
+    @player_contact.current_user = @user
+    @player_contact.master.current_user = @user
+
+    referenced = @player_contact.activity_log__player_contact_elts.create!(select_call_direction: 'from staff',
+                                                                           extra_log_type: 'mr_self_ref',
+                                                                           select_who: 'abc',
+                                                                           master: @player_contact.master)
+
+    referenced2 = @player_contact.activity_log__player_contact_elts.create!(select_call_direction: 'from staff',
+                                                                            extra_log_type: 'mr_self_ref',
+                                                                            select_who: 'abc2',
+                                                                            master: @player_contact.master)
+
+    al_simple = @player_contact.activity_log__player_contact_elts.build(select_call_direction: 'from staff',
+                                                                        extra_log_type: 'mr_self_ref',
+                                                                        select_who: 'abc')
+    al_simple.save!
+
+    mr = ModelReference.create_from_master_with(al_simple.master, @player_contact)
+
+    mr_ref = ModelReference.create_with(al_simple, referenced)
+    mr_ref2 = ModelReference.create_with(al_simple, referenced2)
+
+    expect(al_simple.model_references.length).to be > 0
+
+    # Disabling a reference pointing to a record with no disabled field should work
+    mr.update!(disabled: true)
+    expect(mr.disabled).to be true
+
+    # Disabling a reference having the option also_disable_record also disables the to_record
+    mr_ref.update!(disabled: true)
+    expect(mr_ref.disabled).to be true
+    referenced.reload
+    expect(referenced.disabled).to be true
+
+    # Disabling the to_record should lead to the model reference records pointing to it being disabled
+    referenced2.update!(disabled: true, current_user: @user)
+    referenced2.reload
+    mr_ref2.reload
+    expect(referenced2.disabled).to be true
+    expect(mr_ref2.disabled).to be true
   end
 end
