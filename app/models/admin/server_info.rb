7706a3f95455af37550c70df75258b4efb3b5871
Phil <phil.ayres@consected.com>
2021-12-08 11:08:49 +0000

    Document class

--- app/models/admin/server_info.rb
+++ app/models/admin/server_info.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+#
+# Handle server actions such as restarts and showing
+# server settings to admins to assist with identifying issues
 class Admin::ServerInfo
   AppSettingsVars = %w[
     PageTitle EnvironmentName BaseUrl
