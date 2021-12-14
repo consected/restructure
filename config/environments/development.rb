844ab5ec6e7405aa08669988f4c9abdd8069bcde
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2021-12-14 10:40:43 -0400

    changed some configs for recoverable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.assets.js_compressor = Uglifier.new(harmony: true)
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
