28c6d4c623fb88bb8b5981490d47f0dc949a98e1
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2021-12-14 10:40:43 -0400

    changed some configs for recoverable

--- config/initializers/user_reminders.rb
+++ config/initializers/user_reminders.rb
@@ -21,8 +21,8 @@ Rails.application.config.to_prepare do
   # TODO: should I move to a more specifically named initializer?
   # Set up the Users::PasswordRecovery notifications by setting the class attributes
   Users::PasswordRecovery.password_recovery_defaults = {
-    content: 'server recover password',
-    layout: 'server recover password',
-    subject: 'Recover Password Notification'
+    content: 'server reset password instructions',
+    layout: 'server reset password instructions',
+    subject: 'Reset Password Instructions'
   }
 end
