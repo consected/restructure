a7a412757c3084a8dea30c178050194c3b8b5c35
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2021-12-23 11:26:57 -0600

    cleaned up comments

--- app/models/users/password_recovery.rb
+++ app/models/users/password_recovery.rb
@@ -7,10 +7,10 @@ module Users
       attr_accessor :password_recovery_defaults
     end
 
-    # Set up the notification email when the user tries to recover the password
+    # Set up the notification email when the user tries to reset the password
     # @param [User] user
     def self.notify(user, options)
-      Rails.logger.info("Setting up the notification when a user tries to recover the password. User #{user.email}")
+      Rails.logger.info("Setting up the notification when a user tries to reset the password. User #{user.email}")
       HandlePasswordRecoveryNotificationJob.perform_now(user, options)
     end
 
