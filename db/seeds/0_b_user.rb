09fb758277e536b324f2cd59e903a6fc0baf4cd1
Santiago Alvarez Vargas <santiago.v.alvarez@gmail.com>
2022-01-03 15:27:36 -0400

    added the user access control required for the registration template user

--- db/seeds/0_b_user.rb
+++ db/seeds/0_b_user.rb
@@ -23,16 +23,19 @@ module Seeds
 
       if Settings::AllowUsersToRegister
         # Create registration template user required when users are allowed to register.
-        if User.active.where(email: Settings::DefaultUserTemplateEmail).first
+        if User.active.find_by(email: Settings::DefaultUserTemplateEmail)
           log 'Did not setup registration template user'
         else
-          if (template_user = User.where(email: Settings::DefaultUserTemplateEmail).first)
+          if (template_user = User.find_by(email: Settings::DefaultUserTemplateEmail))
             template_user.update! disabled: false, current_admin: auto_admin
           else
             template_user = User.create!(email: Settings::DefaultUserTemplateEmail, first_name: 'registration', last_name: 'template', current_admin: auto_admin)
             # creates the registration template user role
             app_type = Admin::AppType.find_by(name: 'zeus')
-            Admin::UserRole.add_to_role(template_user, app_type, 'user', auto_admin)
+            role_name = 'user'
+            Admin::UserRole.add_to_role(template_user, app_type, role_name, auto_admin)
+            # TODO: Should creating the UserAccessControl for the registration template user belong here?
+            Admin::UserAccessControl.create!(user: nil, app_type: app_type, resource_type: 'general', resource_name: 'app_type', role_name: role_name, access: :read, current_admin: auto_admin)
           end
           log "Ran #{self}.setup"
         end
