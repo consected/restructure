dbb2b07b0c10dfc898869a37b8c764a4746c0540
Phil <phil.ayres@consected.com>
2022-01-11 13:58:06 +0000

    Updated change user roles to allow app_type to be specified

--- app/models/save_triggers/change_user_roles.rb
+++ app/models/save_triggers/change_user_roles.rb
@@ -20,23 +20,46 @@ class SaveTriggers::ChangeUserRoles < SaveTriggers::SaveTriggersBase
   end
 
   def perform
-    @add_names&.each do |role_name|
+    @add_names&.each do |role_def|
+      role_name, app_type_id = handle_role_def(role_def)
+
       item.current_user.user_roles.create(
-        app_type_id: item.current_user.app_type_id,
+        app_type_id: app_type_id,
         role_name: role_name,
         current_admin: use_admin
       )
     end
 
-    @remove_names&.each do |role_name|
-      item.current_user.user_roles.active.where(
-        app_type_id: item.current_user.app_type_id,
+    @remove_names&.each do |role_def|
+      role_name, app_type_id = handle_role_def(role_def)
+
+      item.current_user.user_roles.active.find_by(
+        app_type_id: app_type_id,
         role_name: role_name
-      ).first&.disable!(use_admin)
+      )&.disable!(use_admin)
     end
   end
 
   private
+
+  def handle_role_def(role_def)
+    if role_def.is_a? Hash
+      app_type = role_def[:app_type]
+      role_name = role_def[:role_name]
+    else
+      role_name = role_def
+    end
+
+    app_type_id = if app_type
+                    Admin::AppType.active_app_types.find_by(name: app_type)&.id
+                  else
+                    item.current_user.app_type_id
+                  end
+
+    raise FphsException, "No active app type found in role definition for app type: #{app_type}" unless app_type_id
+
+    [role_name, app_type_id]
+  end
 
   def use_admin
     Admin.find(1)
