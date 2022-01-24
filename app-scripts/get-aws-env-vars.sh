197a3eb7b9a9d7e286484cda3275a7bd5bdadb2f
Phil <phil.ayres@consected.com>
2022-01-24 21:09:59 +0000

    Updated config script

--- app-scripts/get-aws-env-vars.sh
+++ app-scripts/get-aws-env-vars.sh
@@ -1,4 +1,7 @@
 # Get the AWS environment variables if we are in an AWS elastic beanstalk environment
 if [ -f /opt/elasticbeanstalk/bin/get-config ]; then
   export $(/opt/elasticbeanstalk/bin/get-config --output YAML environment | sed -r 's/: /=/' | xargs)
+  export EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+  export AWS_DEFAULT_REGION="$(echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//')"
+  export AWS_REGION=${AWS_DEFAULT_REGION}
 fi
