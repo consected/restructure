63c0795640e738cc8aab6a8de4a1b51ba9078e0b
Phil <phil.ayres@consected.com>
2022-02-08 16:58:52 +0000

    Fixed pregenerated and non-editable external identifier fields not to show

--- app/models/option_configs/external_identifier_options.rb
+++ app/models/option_configs/external_identifier_options.rb
@@ -3,6 +3,15 @@ module OptionConfigs
     def self.set_defaults(config_obj, all_options = {})
       all_options[:default] ||= {}
       all_options[:default][:fields] ||= config_obj.all_implementation_fields
+
+      never_show = config_obj.pregenerate_ids || config_obj.prevent_edit
+      return unless never_show
+
+      id_field = config_obj.external_id_attribute.to_sym
+      all_options[:default][:show_if] ||= {}
+      all_options[:default][:show_if][id_field] ||= {
+        never: never_show
+      }
     end
 
     def self.raise_bad_configs(option_configs)
