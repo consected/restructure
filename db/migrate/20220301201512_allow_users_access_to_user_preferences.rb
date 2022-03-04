59c468382a61e3a0a2bb35af84e35a1f42510813
Santiago Alvarez Vargas <santiago.v.alvarez@gmail.com>
2022-03-04 11:43:40 -0400

    included in seeds a way for creating the user access control and the page layout for the user preferences

--- db/migrate/20220301201512_allow_users_access_to_user_preferences.rb
+++ db/migrate/20220301201512_allow_users_access_to_user_preferences.rb
@@ -1,24 +1,14 @@
 class AllowUsersAccessToUserPreferences < ActiveRecord::Migration[5.2]
   def up
-    auto_admin = Admin.active.first
-    Admin::UserAccessControl.find_or_create_by!(resource_name: 'user_preferences', resource_type: 'table', access: 'create') do |user_access_control|
-      user_access_control.current_admin = auto_admin
-      user_access_control.disabled = false
-    end
+    $dont_seed = true
 
-    options_yaml = <<~END_OPTIONS
-      contains:
-        resources:
-          - user_preference
-    END_OPTIONS
-
-    Admin::PageLayout.find_or_create_by!(layout_name: 'user_profile',
-                                         panel_name: 'user_profile_all') do |page_layout|
-      page_layout.panel_label = 'User Profile Tab'
-      page_layout.options = options_yaml
-      page_layout.panel_position = 1
-      page_layout.current_admin = auto_admin
-      page_layout.disabled = false
+    begin
+      require "#{::Rails.root}/db/seeds.rb"
+      Seeds::AllowAccessToUserPreferences.setup
+    rescue StandardError => e
+      puts 'Run "bundle exec rails db:seed"" at the end of migrations.'
+      puts e
+      puts e.backtrace.join("\n")
     end
   end
 end
