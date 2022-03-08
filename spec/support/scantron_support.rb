9eccd1c9e50170485e427b90e65245f7702d3488
Phil <phil.ayres@consected.com>
2022-03-08 14:12:46 +0000

    Updated previous spec commit

--- spec/support/scantron_support.rb
+++ spec/support/scantron_support.rb
@@ -1,22 +1,29 @@
 module ScantronSupport
   include MasterSupport
+
+  # Since scantron definition can be redefined after the initial load of this module,
+  # make sure that we use the current version of the model for building tests
+  def current_scantron_model
+    Resources::Models.find_by(resource_name: 'scantrons').model
+  end
+
   def list_valid_attribs
     res = []
 
-    (1..5).each do |l|
+    (1..5).each do |_l|
       res << {
-        scantron_id: rand(999998)+1
+        scantron_id: rand(1..999_998)
       }
     end
-    res << {scantron_id: 999999 }
-    res << {scantron_id: 1 }
+    res << { scantron_id: 999_999 }
+    res << { scantron_id: 1 }
     res
   end
 
   def list_invalid_attribs
     [
       {
-        scantron_id: 1000000
+        scantron_id: 1_000_000
       },
       {
         scantron_id: 'asjfjgsf'
@@ -35,29 +42,24 @@ module ScantronSupport
 
   def new_attribs
     @new_attribs = {
-      scantron_id: rand(999998) + 1
+      scantron_id: rand(1..999_998)
     }
   end
 
-  def delete_previous_records scantron_id=nil
+  def delete_previous_records(scantron_id = nil)
     whereclause = ''
-    whereclause =  "where scantron_id = '#{scantron_id}'" if scantron_id
+    whereclause = "where scantron_id = '#{scantron_id}'" if scantron_id
     ActiveRecord::Base.connection.execute "delete from scantron_history #{whereclause};
     delete from scantrons  #{whereclause};
     "
   end
 
-
-  def create_item att=nil, master=nil, allow_dup=false
-
+  def create_item(att = nil, master = nil, allow_dup = false)
     att ||= valid_attribs
-    unless allow_dup
-      delete_previous_records att[:scantron_id]
-    end
-
+    delete_previous_records att[:scantron_id] unless allow_dup
 
     master ||= create_master
-    @scantron = master.scantrons.create! att
+    att[:master] = master
+    @scantron = current_scantron_model.create! att
   end
-
 end
