99dc434766cccac6a8d5f5809d54155f37bdb0e0
Phil <phil.ayres@consected.com>
2022-03-24 15:10:50 +0000

    Changed handling of AWS API settings on initialization

--- config/initializers/app_settings_aws_api_setup.rb
+++ config/initializers/app_settings_aws_api_setup.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-# Set up AWS messaging default configurations
-ActiveSupport.on_load(:messaging_setup) do
-  self.sms_aws_region = ENV['SMS_AWS_REGION'] || 'us-east-1'
+# Set up AWS API default configurations
+ActiveSupport.on_load(:aws_api_setup) do
+  self.sms_aws_region = ENV['SMS_AWS_REGION'] || ENV['AWS_REGION'] || 'us-east-1'
+  self.s3_aws_region =  ENV['S3_AWS_REGION'] || ENV['AWS_REGION'] || 'us-east-1'
+
   if Rails.env.production?
     self.sender_id = ENV['SMS_SENDER_ID']
     self.importance = (ENV['SMS_IMPORTANCE'] == 'low' ? 'Promotional' : 'Transactional')
