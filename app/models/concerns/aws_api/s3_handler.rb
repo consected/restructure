a0c9ac6e48be6dc7069014ca5d141f0cb6b8e809
Phil <phil.ayres@consected.com>
2022-03-30 16:56:12 +0100

    Fixed use of AWS region

--- app/models/concerns/aws_api/s3_handler.rb
+++ app/models/concerns/aws_api/s3_handler.rb
@@ -1,22 +1,28 @@
 module AwsApi
   module S3Handler
-
     extend ActiveSupport::Concern
 
+    class_methods do
+      def s3_aws_region
+        AwsApiSetup.s3_aws_region
+      end
+    end
 
     # Create and return an S3 client for reuse
     # @return [Aws::S3::Client]
     def aws_s3_client
       return @aws_s3_client if @aws_s3_client
-      @aws_s3_client = Aws::S3::Client.new(region: 'us-east-1')
+
+      @aws_s3_client = Aws::S3::Client.new(region: self.class.s3_aws_region)
     end
 
     # Return the named S3 bucket for use, or if already initialized use the existing bucket
     # @param [bucket_name]
     # @return [Aws::S3::Bucket]
-    def s3_bucket name=nil
+    def s3_bucket(name = nil)
       return @s3_bucket if name.nil? && @s3_bucket || name == @s3_bucket_name && bucket
       return nil unless name
+
       @s3_bucket_name = name
       @s3_bucket = aws_s3_client.get_bucket name
     end
@@ -26,9 +32,7 @@ module AwsApi
     # @param data [String|Byte[]]
     # @param bucket: [String] optional name of bucket. Will reuse the existing bucket name if one is already being used and the param is nil
     # @return [Aws:S3:Object] object uploaded
-    def s3_upload_file filename, data, to_bucket: nil, options: nil
-
-
+    def s3_upload_file(filename, data, to_bucket: nil, options: nil)
       put_info = {
         key: filename,
         body: data,
@@ -37,16 +41,14 @@ module AwsApi
       }
 
       put_info.merge! options if options
-      obj = aws_s3_client.put_object(put_info)
-
-      obj
+      aws_s3_client.put_object(put_info)
     end
 
     # Check if the file exists in S3
     # @param filename [String] filename
     # @param bucket: [String] optional name of bucket. Will reuse the existing bucket name if one is already being used and the param is nil
     # @return [Boolean]
-    def s3_file_exists? filename, bucket: nil
+    def s3_file_exists?(filename, bucket: nil)
       res = aws_s3_client.head_object(
         key: filename,
         bucket: bucket || @s3_bucket_name
@@ -58,7 +60,7 @@ module AwsApi
     # @param filename [String] filename
     # @param bucket: [String] optional name of bucket. Will reuse the existing bucket name if one is already being used and the param is nil
     # @return [Boolean]
-    def s3_file_get_all filename, bucket: nil
+    def s3_file_get_all(filename, bucket: nil)
       res = aws_s3_client.get_object(
         key: filename,
         bucket: bucket || @s3_bucket_name
@@ -72,7 +74,7 @@ module AwsApi
     # @param start_after: [String] optional key to start after. S3 starts listing after this specified key. If a prefix is set but
     # => it doesn't appear in key, it will be added automatically
     # @return [Boolean]
-    def s3_list bucket: nil, prefix: nil, start_after: nil
+    def s3_list(bucket: nil, prefix: nil, start_after: nil)
       cond = {
         bucket: bucket || @s3_bucket_name
       }
@@ -87,7 +89,5 @@ module AwsApi
 
       res.contents
     end
-
-
   end
 end
