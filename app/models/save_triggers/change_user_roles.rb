48ad1e6f58800ad9e62e4b2dfe77256d413ef9d9
Phil <phil.ayres@consected.com>
2022-04-26 16:19:15 +0100

    Added change_user_roles option for_user to specify non-current user, and allow lookup of role names with calc reference

--- app/models/save_triggers/change_user_roles.rb
+++ app/models/save_triggers/change_user_roles.rb
@@ -1,16 +1,10 @@
 # frozen_string_literal: true
 
 class SaveTriggers::ChangeUserRoles < SaveTriggers::SaveTriggersBase
-  attr_accessor :role, :users, :layout_template, :content_template, :message_type, :subject, :receiving_user_ids
+  attr_accessor :role, :users, :layout_template, :content_template, :message_type, :subject, :receiving_user_ids,
+                :app_type_id, :role_name, :for_user
 
-  def self.config_def(if_extras: {})
-    # {
-    #   name: 'part of directory name or array of attribute names to use to generate directory name (value used directly if attribute not found)',
-    #   label: 'human name',
-    #   create_with_role: 'role name',
-    #   if: if_extras
-    # }
-  end
+  def self.config_def(if_extras: {}); end
 
   def initialize(config, item)
     super
@@ -21,9 +15,9 @@ class SaveTriggers::ChangeUserRoles < SaveTriggers::SaveTriggersBase
 
   def perform
     @add_names&.each do |role_def|
-      role_name, app_type_id = handle_role_def(role_def)
+      role_name, app_type_id, for_user = handle_role_def(role_def)
 
-      item.current_user.user_roles.create(
+      for_user.user_roles.create(
         app_type_id: app_type_id,
         role_name: role_name,
         current_admin: use_admin
@@ -31,9 +25,9 @@ class SaveTriggers::ChangeUserRoles < SaveTriggers::SaveTriggersBase
     end
 
     @remove_names&.each do |role_def|
-      role_name, app_type_id = handle_role_def(role_def)
+      role_name, app_type_id, for_user = handle_role_def(role_def)
 
-      item.current_user.user_roles.active.find_by(
+      for_user.user_roles.active.find_by(
         app_type_id: app_type_id,
         role_name: role_name
       )&.disable!(use_admin)
@@ -42,23 +36,47 @@ class SaveTriggers::ChangeUserRoles < SaveTriggers::SaveTriggersBase
 
   private
 
+  #
+  # Handle the job definition using the options
+  #   app_type
+  #   role_name
+  #   for_user (defaults to item.current_user if not set)
+  # If for_user was not set, default to the current user
   def handle_role_def(role_def)
     if role_def.is_a? Hash
       app_type = role_def[:app_type]
-      role_name = role_def[:role_name]
+      self.role_name = role_def[:role_name]
+      self.for_user = role_def[:for_user]
     else
-      role_name = role_def
+      self.role_name = role_def
     end
 
-    app_type_id = if app_type
-                    Admin::AppType.active_app_types.find_by(name: app_type)&.id
-                  else
-                    item.current_user.app_type_id
-                  end
+    self.app_type_id = if app_type
+                         Admin::AppType.active_app_types.find_by(name: app_type)&.id
+                       else
+                         item.current_user.app_type_id
+                       end
 
     raise FphsException, "No active app type found in role definition for app type: #{app_type}" unless app_type_id
 
-    [role_name, app_type_id]
+    self.role_name = FieldDefaults.calculate_default(item, role_name) if role_name
+
+    self.for_user = if for_user
+                      FieldDefaults.calculate_default(item, for_user)
+                    else
+                      item.current_user
+                    end
+
+    self.for_user = case for_user
+                    when Integer
+                      User.active.find_by(id: for_user)
+                    when String
+                      User.active.find_by(email: for_user)
+                    else
+                      for_user
+                    end
+
+    [role_name, app_type_id, for_user]
   end
 
   def use_admin
--- spec/models/save_triggers/change_user_roles_spec.rb
+++ spec/models/save_triggers/change_user_roles_spec.rb
@@ -102,4 +102,77 @@ RSpec.describe SaveTriggers::ChangeUserRoles, type: :model do
 
     expect(@user.user_roles.active.reload.pluck(:role_name)).to eq(['viewer-has-agreement2'])
   end
+
+  it 'changes the roles of the specified user' do
+    @orig_app_type = @user.app_type
+    @alt_app_type = Admin::AppType.active_app_types.last
+    expect(@alt_app_type.id).not_to eq @user.app_type_id
+
+    Admin::UserAccessControl.create app_type: @alt_app_type, role_name: 'user', resource_type: 'general', resource_name: 'app_type', access: 'read', current_admin: @admin
+
+    # Ensure we are using a user that is not the current_user
+    other_user = @user
+    create_user
+
+    config = {
+      add_role_names: [
+        {
+          role_name: 'viewer-has-agreement',
+          app_type: other_user.app_type.name,
+          for_user: other_user.email
+        },
+        {
+          role_name: 'user',
+          app_type: @alt_app_type.name,
+          for_user: {
+            all: {
+              @al.class.resource_name => {
+                id: @al.id,
+                user_id: 'return_value'
+              }
+            }
+
+          }
+
+        }
+      ],
+      remove_role_names: ['viewer-no-agreement']
+    }
+
+    res = other_user.user_roles.pluck(:role_name)
+    expect(res).to be_empty
+    @trigger = SaveTriggers::ChangeUserRoles.new(config, @al)
+    @trigger.perform
+
+    expect(other_user.user_roles.active.reload.pluck(:role_name)).to eq(['viewer-has-agreement'])
+
+    other_user.app_type = @alt_app_type
+    other_user.save!
+
+    expect(other_user.user_roles.active.reload.pluck(:role_name)).to eq(['user'])
+
+    other_user.app_type = @orig_app_type
+    other_user.save!
+
+    config = {
+      add_role_names: ['viewer-has-agreement2'],
+      remove_role_names: [
+        { role_name: 'viewer-no-agreement', app_type: other_user.app_type.name }
+      ]
+    }
+
+    @trigger = SaveTriggers::ChangeUserRoles.new(config, @al)
+    @trigger.perform
+
+    expect(other_user.user_roles.active.reload.pluck(:role_name)).to eq(['viewer-has-agreement', 'viewer-has-agreement2'])
+
+    config = {
+      remove_role_names: ['viewer-has-agreement']
+    }
+
+    @trigger = SaveTriggers::ChangeUserRoles.new(config, @al)
+    @trigger.perform
+
+    expect(other_user.user_roles.active.reload.pluck(:role_name)).to eq(['viewer-has-agreement2'])
+  end
 end
