7afc9d3a8757495acea7c32f6f5b3594c0b02907
Phil <phil.ayres@consected.com>
2022-05-11 10:18:56 +0100

    Updated parallel tests script for reliability

--- app-scripts/parallel_test.sh
+++ app-scripts/parallel_test.sh
@@ -1,26 +1,56 @@
 #!/bin/bash
+echo > log/test.log
+echo > tmp/failing_specs.log
+echo > tmp/working_failing_specs.log
+
 # First, run brakeman
 if [ "${NO_BRAKEMAN}" != 'true' ]; then
+  echo "Running brakeman"
   bin/brakeman -q --summary > /tmp/fphs-brakeman-summary.txt
   cat /tmp/fphs-brakeman-summary.txt
 fi
+
 echo "Setup filestore"
 app-scripts/setup-dev-filestore.sh
-echo > log/test.log
+
 # Run the rspec tests in parallel. Use the first arg to define the path if needed
 export PARALLEL_TEST_PROCESSORS=${PARALLEL_TEST_PROCESSORS:=$(nproc)}
 
 if [ -z "$@" ]; then
-  specs='spec/controllers spec/features spec/javascripts spec/jobs spec/models spec/requests spec/routing'
+  specs='spec/models spec/controllers spec/features spec/j.* spec/r.*'
 else
   specs=$@
 fi
 
-rm -f tmp/working_failing_specs.log
-
 for spec in ${specs}; do
-  bundle exec rake parallel:spec[${spec}]
+  echo "========================================================================"
+  echo "==>>>> Running parallel specs for '${spec}'"
+  echo "========================================================================"
+  echo "========================================================================" >> tmp/working_failing_specs.log
+  echo "==>>>> Running parallel specs for '${spec}'" >> tmp/working_failing_specs.log
+  echo "========================================================================" >> tmp/working_failing_specs.log
+  bundle exec rake parallel:spec["'"${spec}"'"] &
+  while ! pgrep -f 'ruby bin/rspec' > /dev/null; do
+    sleep 5
+  done
+  sleep 2
+  # Display the running specs
+  ps aux | grep 'ruby bin/rspec'
+
+  # Wait for all specs to finish
+  while pgrep -f 'ruby bin/rspec' > /dev/null; do
+    sleep 5
+  done
+  # Kill the locked parent
+  sleep 5
+  if [ "$(pgrep -f 'bin/parallel_test')" ]; then
+    kill $(pgrep -f 'bin/parallel_test')
+  fi
   cat tmp/failing_specs.log >> tmp/working_failing_specs.log
 done
 
+echo "========================================================================" >> tmp/working_failing_specs.log
+echo "All Done" >> tmp/working_failing_specs.log
+echo "Runs with Failures: $(grep 'Failures: ' tmp/failing_specs.log | wc -l)" >> tmp/working_failing_specs.log
+echo "========================================================================" >> tmp/working_failing_specs.log
 mv tmp/working_failing_specs.log tmp/failing_specs.log
