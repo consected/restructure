5f8783c0cfb2ae53cce98af87c4662bba3008261
Phil <phil.ayres@consected.com>
2022-11-03 15:42:05 +0000

    Fixed script issue

--- app-scripts/create-test-db.sh
+++ app-scripts/create-test-db.sh
@@ -24,12 +24,14 @@ function setup() {
 
   if [ "${USE_PG_HOST}" ]; then
     USE_PG_UNAME=${USE_PG_UNAME:=postgres}
-    psql -c "create database $DBNAME; create extension if not exists pgcrypto;" -U ${USE_PG_UNAME} -h "${USE_PG_HOST}"
+    psql -c "create extension if not exists pgcrypto;" -U ${USE_PG_UNAME} -h "${USE_PG_HOST}"
+    psql -c "create database $DBNAME;"
     psql -d $DBNAME -U ${USE_PG_UNAME} -h "${USE_PG_HOST}" < "../db/structure.sql"
     psql -d $DBNAME -c "create schema if not exists bulk_msg;" -U ${USE_PG_UNAME} -h "${USE_PG_HOST}"
     psql -d $DBNAME -c "create schema if not exists ref_data;" -U ${USE_PG_UNAME} -h "${USE_PG_HOST}"
   else
-    sudo -u postgres psql -c "create database $DBNAME with owner $DBOWNER; create extension if not exists pgcrypto;"
+    sudo -u postgres psql -c "create extension if not exists pgcrypto;"
+    sudo -u postgres psql -c "create database $DBNAME with owner $DBOWNER;"
     psql -d $DBNAME < "../db/structure.sql"
     psql -d $DBNAME -c "create schema if not exists bulk_msg;"
     psql -d $DBNAME -c "create schema if not exists ref_data;"
