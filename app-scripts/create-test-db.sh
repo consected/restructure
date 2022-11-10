e8e45c1b41e10b59e7792182b06d06efc0ca513e
Phil <phil.ayres@consected.com>
2022-11-10 17:27:47 +0000

    Fixed create script

--- app-scripts/create-test-db.sh
+++ app-scripts/create-test-db.sh
@@ -24,12 +24,16 @@ function setup() {
 
   if [ "${USE_PG_HOST}" ]; then
     USE_PG_UNAME=${USE_PG_UNAME:=postgres}
-    psql -c "create database $DBNAME; create extension if not exists pgcrypto;" -U ${USE_PG_UNAME} -h "${USE_PG_HOST}"
+    psql -c "create extension if not exists pgcrypto;" -U ${USE_PG_UNAME} -h "${USE_PG_HOST}"
+    psql -c "create database $DBNAME;"
+    psql -c "create user fphsetl password 'fphs'; create user fphs password 'fphs';"
     psql -d $DBNAME -U ${USE_PG_UNAME} -h "${USE_PG_HOST}" < "../db/structure.sql"
     psql -d $DBNAME -c "create schema if not exists bulk_msg;" -U ${USE_PG_UNAME} -h "${USE_PG_HOST}"
     psql -d $DBNAME -c "create schema if not exists ref_data;" -U ${USE_PG_UNAME} -h "${USE_PG_HOST}"
   else
-    sudo -u postgres psql -c "create database $DBNAME with owner $DBOWNER; create extension if not exists pgcrypto;"
+    sudo -u postgres psql -c "create extension if not exists pgcrypto;"
+    sudo -u postgres psql -c "create database $DBNAME with owner $DBOWNER;"
+    sudo -u postgres psql -c "create user fphsetl password 'fphs'; create user fphs password 'fphs';"
     psql -d $DBNAME < "../db/structure.sql"
     psql -d $DBNAME -c "create schema if not exists bulk_msg;"
     psql -d $DBNAME -c "create schema if not exists ref_data;"
