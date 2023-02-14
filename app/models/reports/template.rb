6a1f4d5875e44918f4862047ebb7a20aedb07987
Phil <phil.ayres@consected.com>
2023-02-14 15:51:16 +0000

    Fixed rendering of templates for embedded reports and e-signed documents

--- app/models/reports/template.rb
+++ app/models/reports/template.rb
@@ -18,23 +18,13 @@ module Reports
       result_tables = runner.result_tables_by_index || []
       outer_block_id = "report-container-#{SecureRandom.hex}"
 
-      # Rails 5 or before, constructor was an argument with nil default value.
-      # Rails 6, controller is required
-      controller = nil
-      view = ActionView::Base.new(ActionController::Base.view_paths, {}, controller)
-      view.class_eval do
-        include ApplicationHelper
-        include ReportsHelper
-        include ReportResults::ReportsCommonHelper
-        include ReportResults::ReportsTableHelper
-      end
-      view.render(partial: 'reports/result_template/table',
-                  locals: {
-                    result_tables: result_tables,
-                    report: report,
-                    results: results,
-                    outer_block_id: outer_block_id
-                  }).html_safe
+      ApplicationController.render(partial: 'reports/result_template/table',
+                                   locals: {
+                                     result_tables: result_tables,
+                                     report: report,
+                                     results: results,
+                                     outer_block_id: outer_block_id
+                                   }).html_safe
     end
   end
 end
