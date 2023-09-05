2c76fe4b2f415b3c2ae9d957bf89ed9ff633a82d
Restructure Build Process <phil_ayres@hms.harvard.edu>
2023-09-05 12:25:42 +0000

    Built and tested release-ready version '8.2.66'

---
+++ vendor/cache/simple_token_authentication-04db492a24a6/spec/lib/simple_token_authentication/exception_fallback_handler_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe SimpleTokenAuthentication::ExceptionFallbackHandler do
+
+  let(:exception_fallback_handler) { SimpleTokenAuthentication::ExceptionFallbackHandler.instance }
+
+  it_behaves_like 'a fallback handler'
+
+  describe '#fallback!' do
+
+    context 'when authentication failed' do
+
+      before(:each) do
+        @entity = double()
+        allow(@entity).to receive_message_chain(:name_underscore).and_return('entity')
+        @controller = double()
+
+        allow(@controller).to receive(:current_entity).and_return(nil)
+      end
+
+      it 'delegates exception throwing to Warden', private: true do
+        expect{ exception_fallback_handler.fallback!(@controller, @entity) }.to throw_symbol(:warden, scope: :entity)
+      end
+    end
+
+    context 'when authentication was successful' do
+
+      before(:each) do
+        @entity = double()
+        allow(@entity).to receive_message_chain(:name_underscore).and_return('entity')
+        @controller = double()
+
+        allow(@controller).to receive(:current_entity).and_return('some entity')
+      end
+
+      it 'does not throw any exception', private: true do
+        expect{ exception_fallback_handler.fallback!(@controller, @entity) }.not_to throw_symbol(:warden, scope: :entity)
+      end
+    end
+  end
+end
