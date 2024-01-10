563817069060c9f1277d0078d3d0ba4ac832e81f
Phil <phil.ayres@consected.com>
2024-01-10 17:45:24 +0000

    [Fixed] upverson bug

--- app-scripts/upversion.rb
+++ app-scripts/upversion.rb
@@ -7,7 +7,7 @@ vs = v.strip.split('.')
 if ARGV.include?('minor')
   # Update the minor version and leave off the patch version
   # e.g. 3.47.89 becomes 3.48
-  vs = vs.delete_at(2)
+  vs.delete_at(2)
   vs[1] = vs[1].to_i + 1
 elsif vs[2].nil?
   # Set the first patch version to zero
