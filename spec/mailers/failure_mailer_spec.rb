f1da18e5a4e5bf2e0d144a2d2138ee7e7a5f826a
Phil <phil.ayres@consected.com>
2024-02-01 12:01:06 +0000

    [Fixed] background Job failures still not notifying the admin via email - fixes #258

--- spec/mailers/failure_mailer_spec.rb
+++ spec/mailers/failure_mailer_spec.rb
@@ -3,6 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe FailureMailer, type: :mailer do
+  include MasterSupport
+  include ModelSupport
+  include PlayerContactSupport
+  include BulkMsgSupport
+  include DynamicModelSupport
+
   describe 'notify job failures' do
     let(:job) { 'Test Job #123' }
     let(:mail) { FailureMailer.notify_job_failure(job).deliver_now }
@@ -21,6 +27,24 @@ RSpec.describe FailureMailer, type: :mailer do
     it 'assigns @confirmation_url' do
       expect(mail.body.encoded)
         .to match("A failure occurred running a delayed_job on server #{Settings::EnvironmentName}.\r\n#{job}")
+    end
+  end
+
+  describe 'test in job' do
+    before :example do
+      # Seeds.setup
+
+      @user0, = create_user
+      create_admin
+      create_user
+
+      import_bulk_msg_app
+    end
+
+    it 'exercises a real job failure' do
+      expect do
+        HandleBatchJob.perform_now('DynamicModel::ZeusBulkMessage', limit: 'fake')
+      end.to raise_error(ArgumentError)
     end
   end
 end
