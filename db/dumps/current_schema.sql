8c9ef36d05e1723b71e54c31da8a4b934f26fd0e
Restructure Build Process <phil_ayres@hms.harvard.edu>
2024-02-20 13:59:14 +0000

    Built and tested release-ready version '8.5.1' - dev repo

--- db/dumps/current_schema.sql
+++ db/dumps/current_schema.sql
@@ -3,8 +3,8 @@ begin;
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 12.15
--- Dumped by pg_dump version 12.15
+-- Dumped from database version 15.5
+-- Dumped by pg_dump version 15.5
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
