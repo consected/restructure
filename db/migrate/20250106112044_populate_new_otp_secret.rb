0514ca446df61e7dfbd370a8df54184ca563a515
Phil <phil.ayres@consected.com>
2025-01-06 19:42:10 +0000

    Added new otp_secret field for devise-two-factor gem

---
+++ db/migrate/20250106112044_populate_new_otp_secret.rb
@@ -0,0 +1,16 @@
+class PopulateNewOtpSecret < ActiveRecord::Migration[7.2]
+  def up
+    admin = Admin.find_by email: Settings::AdminEmail.downcase
+
+    User.find_each do |user| # find_each finds in batches of 1,000 by default
+      otp_secret = user.otp_secret # read from otp_secret column, fall back to legacy columns if new column is empty
+      puts "Processing #{user.email} - #{user.first_name} #{user.last_name}"
+      res = user.update(otp_secret: otp_secret, current_admin: admin)
+      unless res
+        msg = "Failed to process 2FA update for #{user.email} - #{user.first_name} #{user.last_name}"
+        puts msg
+        Rails.logger.error msg
+      end
+    end
+  end
+end
