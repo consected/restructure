14945eb08e3c80ee9e9072b326ab3722d06fb7e3
Phil <phil.ayres@consected.com>
2025-01-06 19:35:43 +0000

    Updated configs for Rails 7

--- config/application.rb
+++ config/application.rb
@@ -1,21 +1,19 @@
-# frozen_string_literal: true
-
 require_relative 'boot'
 
-# Include each railties manually, excluding `active_storage/engine`
-# action_cable/engine
-%w[
-  active_record/railtie
-  action_controller/railtie
-  action_view/railtie
-  action_mailer/railtie
-  active_job/railtie
-  rails/test_unit/railtie
-  sprockets/railtie
-].each do |railtie|
-  require railtie
-rescue LoadError
-end
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+# require "active_storage/engine"
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require 'action_view/railtie'
+# require "action_cable/engine"
+# require "rails/test_unit/railtie"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -23,14 +21,15 @@ Bundler.require(*Rails.groups)
 module Fpa1
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
-    config.autoloader = :zeitwerk
+    config.load_defaults 7.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
     config.active_record.schema_format = :sql
   end
 end
