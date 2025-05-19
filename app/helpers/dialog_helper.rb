7ff1f4a8e1a52180e8fd239312bc2c0c74aee0ac
Phil <phil.ayres@consected.com>
2025-05-19 17:44:34 +0100

    Fixed bug suggesting new dialog to add

--- app/helpers/dialog_helper.rb
+++ app/helpers/dialog_helper.rb
@@ -4,6 +4,7 @@ module DialogHelper
     if dialog 
       txt = link_to link_label_open_in_new("#{dialog.category} - #{dialog.name}"), admin_message_templates_path(filter: {id: dialog.id}, perform_action: 'edit'), target: "_blank"
     else
+      name = dialog&.name || config_or_name[:name] || config_or_name['name']
       txt = <<~END_HTML 
         Add a #{link_to( link_label_open_in_new("new dialog for '#{name}'"), admin_message_templates_path(init_with: {message_type: 'dialog', template_type: 'content', category: object_instance.category, name: name }, perform_action: 'new'), target: "_blank").html_safe }
         with <i>message type:</i> <b>dialog</b>, <i>template type:</i> <b>content</b> &amp;  <i>name:</i> <b>#{ name }</b>
