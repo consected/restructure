60ab94b5957c46ae400e41d48c66a3f093f60014
Phil <phil.ayres@consected.com>
2025-06-26 17:20:31 +0100

    Added a mechanism to copy all the sub_process, protocol_event tree from one protocol to another - resolves #578

--- spec/models/classification/protocol_spec.rb
+++ spec/models/classification/protocol_spec.rb
@@ -55,4 +55,119 @@ describe Classification::Protocol do
       expect(p.sub_processes.pluck(:name)).to include('Activity')
     end
   end
+  describe '#copy_from' do
+    def get_clean_protocol(protocol)
+      protocol = protocol ? Classification::Protocol.find(protocol.id) : create_item
+      protocol.current_admin = @admin
+      protocol
+    end
+
+    def check_protocols_match(source, target)
+      # Check sub_processes were copied
+      expect(target_protocol.sub_processes.enabled.count).not_to eq(0)
+      expect(target_protocol.sub_processes.enabled.reload.count).to eq(source_protocol.sub_processes.enabled.reload.count)
+
+      # Check each sub_process and its events
+      target_protocol.sub_processes.enabled.reload.each_with_index do |sub, i|
+        source_sub = source_protocol.sub_processes.reload.find_by(name: sub.name)
+
+        # Check sub_process attributes
+        expect(sub.name).to eq(source_sub.name)
+        expect(sub.disabled).to eq(source_sub.disabled)
+
+        # Check protocol_events
+        expect(sub.protocol_events.enabled.reload.count).not_to eq(0)
+        sub.protocol_events.enabled.reload.each_with_index do |event, j|
+          source_event = source_sub.protocol_events.enabled.reload.find_by(name: event.name)
+
+          expect(event.name).to eq(source_event.name)
+          expect(event.disabled).to eq(source_event.disabled)
+          expect(event.description).to eq(source_event.description)
+          expect(event.milestone).to eq(source_event.milestone)
+        end
+      end
+    end
+
+    let(:source_protocol) { @source_protocol = get_clean_protocol(@source_protocol) }
+    let(:target_protocol) { @target_protocol = get_clean_protocol(@target_protocol) }
+
+    def add_protocols_and_sub_processes(state)
+      2.times do |i|
+        sub_process = source_protocol.sub_processes.create!(
+          name: "Sub Process #{state} #{i}",
+          current_admin: @admin,
+          disabled: false
+        )
+
+        2.times do |j|
+          sub_process.protocol_events.create!(
+            name: "Event #{i}-#{j}",
+            # position: j,
+            disabled: false,
+            # event_type: 'test_type',
+            # options: { key: "value-#{i}-#{j}" },
+            current_admin: @admin
+          )
+        end
+      end
+    end
+
+    before do
+      create_admin
+      # Create sub_processes with events for source protocol
+      add_protocols_and_sub_processes 'init'
+    end
+
+    it 'copies all sub_processes and protocol_events' do
+      source_protocol.sub_processes.reload
+      target_protocol.sub_processes.reload
+      res = target_protocol.copy_from(source_protocol)
+      # puts res.to_yaml
+      expect(res.length).to eq 2
+      expect(res).to eq(
+        'Sub Process init 1' => ['Event 1-0', 'Event 1-1'],
+        'Sub Process init 0' => ['Event 0-0', 'Event 0-1']
+      )
+      check_protocols_match(source_protocol, target_protocol)
+    end
+
+    it 'updates sub_processes and protocol_events if new ones are added in the source protocol' do
+      source_protocol.sub_processes.reload
+      target_protocol.sub_processes.reload
+      res = target_protocol.copy_from(source_protocol)
+      expect(res.length).to eq 2
+      # puts res.to_yaml
+      check_protocols_match(source_protocol, target_protocol)
+
+      add_protocols_and_sub_processes 'update'
+      source_protocol.sub_processes.reload
+      target_protocol.sub_processes.reload
+
+      res = target_protocol.copy_from(source_protocol)
+      expect(res.length).to eq 2
+      # puts res.to_yaml
+      check_protocols_match(source_protocol, target_protocol)
+
+      # Nothing new should be added if no changes in source
+      res = target_protocol.copy_from(source_protocol)
+      expect(res).to be_empty
+      # puts res.to_yaml
+      check_protocols_match(source_protocol, target_protocol)
+    end
+
+    it 'raises exception when copying from same protocol' do
+      expect { target_protocol.copy_from(target_protocol) }.to raise_error(FphsException, "Can't copy protocol sub processes to self")
+    end
+
+    it 'maintains data consistency in transaction' do
+      allow(target_protocol.sub_processes).to receive(:create!).and_raise('Test error')
+
+      expect do
+        target_protocol.copy_from(source_protocol)
+      end.to raise_error('Test error')
+
+      # Verify no partial data was created
+      expect(target_protocol.sub_processes.count).to eq(0)
+    end
+  end
 end
