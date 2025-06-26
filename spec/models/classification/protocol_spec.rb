848a47e431ae5c1c9dc957653f893a7e6990ea66
Phil <phil.ayres@consected.com>
2025-06-26 15:00:39 +0100

    Cleaned rspec to start

--- spec/models/classification/protocol_spec.rb
+++ spec/models/classification/protocol_spec.rb
@@ -2,61 +2,57 @@ require 'rails_helper'
 
 describe Classification::Protocol do
   include ModelSupport
-  include ProtocolSupport  
-  describe "definition" do
+  include ProtocolSupport
+  describe 'definition' do
     before :each do
       seed_database
       create_user
       create_admin
-      create_master      
+      create_master
       create_items :list_valid_attribs
     end
-    
-    it "allows multiple Protocols to be created and returned in order based on position" do
-      
-      
+
+    it 'allows multiple Protocols to be created and returned in order based on position' do
       expect(@created_count).to eq @list.length
-      
+
       Classification::Protocol.all.each do |p|
         p.position = rand 100
         p.current_admin = @admin
         p.save!
       end
-      
+
       prev_pos = -1
-      Classification::Protocol.all.each do |p|                      
+      Classification::Protocol.all.each do |p|
         expect(p.position).to be >= prev_pos
-        prev_pos  = p.position if p.position
+        prev_pos = p.position if p.position
       end
-      
+
       expect(prev_pos).to be > 0
-      
     end
-    
-    it "can return active items only" do
+
+    it 'can return active items only' do
       pa = Classification::Protocol.active
       expect(pa.length).to be > 0
-      res = pa.select {|p| p.disabled }
+      res = pa.select { |p| p.disabled }
       expect(res.length).to eq 0
     end
-    
-    it "can only have name updated by an admin" do
+
+    it 'can only have name updated by an admin' do
       pa = Classification::Protocol.active.first
-      pa.name = "new name by me"
-      
+      pa.name = 'new name by me'
+
       expect(pa.save).to be false
-      
+
       pa.current_admin = @admin
       expect(pa.save).to be true
-      
+
       pa.reload
-      expect(pa.name).to eq "new name by me"
+      expect(pa.name).to eq 'new name by me'
     end
-    
+
     it 'updates activity log tracker events when a protocol is created' do
       p = create_item name: "Test tracker events #{Time.now.to_f}"
       expect(p.sub_processes.pluck(:name)).to include('Activity')
     end
-
   end
 end
