c7711aa083668cd1bada69a7d2ecc944c3963055
Phil <phil.ayres@consected.com>
2025-08-18 17:36:55 +0100

    Fixed create_master with move_this breaks if there is an embedded item that has no master association and provide new specs - fixes #635

--- spec/models/save_triggers/create_master_spec.rb
+++ spec/models/save_triggers/create_master_spec.rb
@@ -10,27 +10,87 @@ RSpec.describe SaveTriggers::CreateMaster, type: :model do
     last_msid = (Master.order(msid: :desc).first.msid || 123) + 1
   end
 
-  before :example do
-    SetupHelper.setup_al_player_contact_phones
-    SetupHelper.setup_al_gen_tests AlNameGenTestCr, 'elt_save_test', 'player_contact'
-    create_user
-    @master = create_master
-    @player_contact = @master.player_contacts.create! data: '(617)123-1234 b', rec_type: :phone, rank: 10
-    @al = create_item master: @master
-    add_reference_def_to(@al, [player_contacts: { from: 'this', add: 'many' }])
-    expect(@al.master_id).to eq @master.id
-    setup_access @al.resource_name, resource_type: :activity_log_type, access: :create, user: @user
+  def add_embedded_item
+    pn = random_phone_number
+    pc_hash = {
+      rec_type: :phone,
+      rank: 10,
+      data: pn
+    }
+    @al.create_embedded_item(pc_hash)
   end
 
-  it 'creates a master record after saving the current one' do
-    pn = random_phone_number
+  describe 'activity logs with player contact referenced' do
+    before :example do
+      SetupHelper.setup_al_player_contact_phones
+      SetupHelper.setup_al_gen_tests AlNameGenTestCr, 'elt_save_test', 'player_contact'
+      create_user
+      @master = create_master
+      @player_contact = @master.player_contacts.create! data: '(617)123-1234 b', rec_type: :phone, rank: 10
+      @al = create_item master: @master
+      add_reference_def_to(@al, [player_contacts: { from: 'this', add: 'one_to_this' }])
+      expect(@al.master_id).to eq @master.id
+      setup_access @al.resource_name, resource_type: :activity_log_type, access: :create, user: @user
+    end
 
-    config = {
-      with: { msid: last_msid }
-    }
-    @trigger = SaveTriggers::CreateMaster.new(config, @al)
-    @trigger.perform
+    it 'creates a master record after saving the current one' do
+      config = {
+        with: { msid: last_msid }
+      }
+      @trigger = SaveTriggers::CreateMaster.new(config, @al)
+      @trigger.perform
 
-    expect(Master.last).to eq @al.save_trigger_results['created_master']
+      new_master = @al.save_trigger_results['created_master']
+      expect(@master.id).not_to eq new_master.id
+      expect(Master.last).to eq new_master
+    end
+
+    it 'creates a master record and moves the current item and embedded item to the new master' do
+      add_embedded_item
+      config = {
+        with: { msid: last_msid },
+        move_this: true
+      }
+      @trigger = SaveTriggers::CreateMaster.new(config, @al)
+      @trigger.perform
+
+      new_master = @al.save_trigger_results['created_master']
+      expect(@master.id).not_to eq new_master.id
+      expect(Master.last).to eq new_master
+
+      expect(@al.master_id).to eq new_master.id
+      expect(@al.embedded_item.master_id).to eq new_master.id
+    end
+  end
+
+  describe 'activity logs with player contact referenced' do
+    before :example do
+      SetupHelper.setup_al_player_contact_phones
+      SetupHelper.setup_al_gen_tests AlNameGenTestCr, 'elt_save_test', 'player_contact'
+      create_user
+      @master = create_master
+      @player_contact = @master.player_contacts.create! data: '(617)123-1234 b', rec_type: :phone, rank: 10
+      @al = create_item master: @master
+      add_reference_def_to(@al, [users: { from: 'this', add: 'one_to_this' }])
+      expect(@al.master_id).to eq @master.id
+      setup_access @al.resource_name, resource_type: :activity_log_type, access: :create, user: @user
+    end
+    it 'creates a master record and moves the current item and embedded item to the new master' do
+      # Add a reference to the current user. The user has no master association, so works for this example
+      ModelReference.create_with(@al, @user)
+
+      config = {
+        with: { msid: last_msid },
+        move_this: true
+      }
+      @trigger = SaveTriggers::CreateMaster.new(config, @al)
+      @trigger.perform
+
+      new_master = @al.save_trigger_results['created_master']
+      expect(@master.id).not_to eq new_master.id
+      expect(Master.last).to eq new_master
+
+      expect(@al.master_id).to eq new_master.id
+    end
   end
 end
