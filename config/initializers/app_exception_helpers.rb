732fa8f43ef8e5ee12ea325ecdf4bd03e15ed43d
Phil <phil.ayres@consected.com>
2025-08-19 18:14:58 +0100

    Changed short string backtrace to help specs

--- config/initializers/app_exception_helpers.rb
+++ config/initializers/app_exception_helpers.rb
@@ -1,7 +1,7 @@
 module ExceptionExtensions
   def short_string_backtrace
     backtrace
-      .select { |m| m.include?(Rails.root.join('app').to_s) }
+      .select { |m| m.include?(Rails.root.join('app').to_s) || m.include?(Rails.root.join('spec').to_s) }
       .join("\n")
   end
 
