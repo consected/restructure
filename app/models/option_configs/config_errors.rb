614443db891d2b832c0870ec250128f96278c801
Phil <phil.ayres@consected.com>
2025-08-21 13:40:43 +0100

    Changed reporting of option config errors and report any errors during cleaning with a separate exception class

--- app/models/option_configs/config_errors.rb
+++ app/models/option_configs/config_errors.rb
@@ -14,20 +14,25 @@ module OptionConfigs
         # @todo
       end
 
-      def failed_config(target, type, message, extra_details: nil, level: :error)
-        rn = resource_name if respond_to?(:resource_name)
-        crn = @config_obj.class&.resource_name if @config_obj
-        cd = { type.to_s => send(type)&.deep_stringify_keys } if respond_to?(type)
+      def failed_config(from_options, target, type, message,
+                        name = nil, resource_name = nil,
+                        extra_details: nil, level: :error)
+        config_obj = from_options&.config_obj
+        name ||= self.name if respond_to?(:name)
+        resource_name ||= self.resource_name if respond_to?(:resource_name)
+        crn = config_obj.class&.resource_name if config_obj
+        option_variable = from_options.instance_variable_get("@#{type}")
+        cd = { type.to_s => option_variable&.deep_stringify_keys } if option_variable.is_a?(Hash)
         cd ||= {}
       ensure
         target << {
           type: type,
-          config_class: @config_obj.class.name,
+          config_class: config_obj.class.name,
           name: name,
           message: message,
-          resource_name: rn,
+          resource_name: resource_name,
           config_resource_name: crn,
-          config_object: @config_obj,
+          config_object: config_obj,
           config_def: cd,
           extra_details:
         }
@@ -37,41 +42,53 @@ module OptionConfigs
       # Return list of config errors across all option_configs, or nil if there are none
       # @param [option_configs] option_configs
       # @return [true | nil]
-      def all_option_configs_errors(option_configs)
-        return unless option_configs
-
-        res = []
-        option_configs.select do |oc|
-          res += oc.config_errors
-        end
-        res = nil if res.empty?
-        res
+      def all_option_configs_errors(object_instance_or_config)
+        all_option_configs_notices(object_instance_or_config, levels: %i[errors])
       end
 
       #
       # Return list of config errors and warnings across all option_configs, or nil if there are none
       # @param [object_instance] object_instance
+      # @param [Array[Symbol]] levels to return (:warnings, :errors)
       # @return [true | nil]
-      def all_option_configs_notices(object_instance)
-        return unless object_instance
+      def all_option_configs_notices(object_instance_or_config, levels: nil)
+        return unless object_instance_or_config
 
+        levels ||= %i[warnings errors]
         begin
-          option_configs = object_instance.option_configs(raise_bad_configs: true)
+          option_configs = if object_instance_or_config.respond_to?(:option_configs)
+                             object_instance = object_instance_or_config
+                             object_instance_or_config.option_configs(raise_bad_configs: [FphsOptionsParseError,
+                                                                                          FphsOptionsGeneralError])
+                           else
+                             object_instance_or_config
+                           end
         rescue StandardError => e
-          msg = "Error retrieving option_configs for #{object_instance.class.name}/#{object_instance.id}: #{e}"
+          msg = if object_instance
+                  "Error retrieving option_configs for #{object_instance.class.name}/#{object_instance.id}: #{e}"
+                else
+                  "Error retrieving option_configs: #{e}"
+                end
           Rails.logger.error msg
           Rails.logger.error e.short_string_backtrace
           res = []
-          failed_config(res, :parse_yaml_config, msg, extra_details: e.backtrace.join("\n"), level: :error)
+          bt = e.short_string_backtrace.presence || e.backtrace.join("\n")
+          failed_config(nil, res, :parse_error, msg, 'YAML options', nil, extra_details: bt,
+                                                                          level: :error)
           return res
         end
 
         res = []
         option_configs.select do |oc|
-          val = oc.config_errors
-          res += val if val
-          val = oc.config_warnings
-          res += val if val
+          if levels.include?(:errors)
+            val = oc.config_errors
+            res += val if val
+          end
+
+          if levels.include?(:warnings)
+            val = oc.config_warnings
+            res += val if val
+          end
         end
         res = nil if res.empty?
         res
@@ -89,7 +106,7 @@ module OptionConfigs
       config.keys.empty? || (config.keys - valid_keys).empty?
     end
 
-    def failed_config(type, message, level: :error)
+    def failed_config(type, message, extra_details: nil, level: :error)
       target = if level == :error
                  config_errors
                elsif level == :warn
@@ -97,7 +114,7 @@ module OptionConfigs
                else
                  config_errors
                end
-      self.class.failed_config(target, type, message, level:)
+      self.class.failed_config(self, target, type, message, name, resource_name, extra_details:, level:)
     end
   end
 end
