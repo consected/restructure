1d5503238a9e78adfecc9992bf0a6fb5f844b75e
Restructure Build Process <phil_ayres@hms.harvard.edu>
2025-12-02 16:24:53 +0000

    Built and tested release-ready version '9.35.1' - dev repo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,8 @@ Since [version 8.4.0](#840---2024-01-10) the convention is that releases made wi
 
 ## Unreleased
 
+## [9.35.1] - 2025-12-02
+
 - [Build] Viva version
 
 ## [9.35.0] - 2025-12-02
--- db/dumps/current_schema.sql
+++ db/dumps/current_schema.sql
@@ -3,7 +3,7 @@ begin;
 -- PostgreSQL database dump
 --
 
-\restrict LawC3X96JDEkI1QGfKQxOJdp0WezBFHe3uVMJ12hXCOG2QtuiCFxbdVOHoXMejr
+\restrict jT0Nc3Ovozkp3uRuDXa0TeF3bBzKVjtdZrXIJuBv981Ia2x76xOT4SMR2unv5F3
 
 -- Dumped from database version 15.14
 -- Dumped by pg_dump version 15.14
@@ -13575,6 +13575,6 @@ ALTER TABLE ONLY ref_data.redcap_data_dictionary_history
 -- PostgreSQL database dump complete
 --
 
-\unrestrict LawC3X96JDEkI1QGfKQxOJdp0WezBFHe3uVMJ12hXCOG2QtuiCFxbdVOHoXMejr
+\unrestrict jT0Nc3Ovozkp3uRuDXa0TeF3bBzKVjtdZrXIJuBv981Ia2x76xOT4SMR2unv5F3
 
 commit;
