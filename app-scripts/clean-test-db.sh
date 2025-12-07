1389f29c5b923e792d4374d09d6bd5abc991b456
Phil <phil.ayres@consected.com>
2025-12-07 10:50:04 +0000

    Fixed test scripts

--- app-scripts/clean-test-db.sh
+++ app-scripts/clean-test-db.sh
@@ -2,7 +2,7 @@
 # Quickly clean and reset the test database (just a single database, not all for parallel tests)
 
 
-USE_PG_UNAME=${USE_PG_UNAME:=$(whoami)}
+export USE_PG_UNAME=${USE_PG_UNAME:=$(whoami)}
 echo "Cleaning a single test database with user Postgres user: ${USE_PG_UNAME}"
 app-scripts/drop-test-db.sh 1
 app-scripts/create-test-db.sh 1
