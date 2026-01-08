257383e7e10675d8626020c5b10a2caa35034c26
Phil <phil.ayres@consected.com>
2026-01-08 13:32:52 +0000

    Fixed batch and save triggers not running the full set of triggers - fixes #816

--- app/models/option_configs/extra_option_implementers/save_triggers.rb
+++ app/models/option_configs/extra_option_implementers/save_triggers.rb
@@ -129,7 +129,8 @@ module OptionConfigs
             all_iter_configs = iter_configs[:do]
             all_iter_configs = [all_iter_configs] unless all_iter_configs.is_a? Array
             all_iter_configs.each do |iter_config|
-              res &&= calc_triggers_for_action(obj, action, iter_config)
+              result = calc_triggers_for_action(obj, action, iter_config)
+              res &&= result
             end
           end
         end
