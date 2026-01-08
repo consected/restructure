44ca3cccf7794eb2ca70ad8577e7d19ce5298895
Phil <phil.ayres@consected.com>
2026-01-08 13:43:34 +0000

    Fixed create_reference force_valid: true option not working for standard "player" models - fixes #818

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,8 +5,8 @@ class Address < UserBase
   include RankHandler
   include ViewHandlers::Address
 
-  validates :source, 'validates/source' => true, allow_blank: true
-  validates :rank, presence: true
+  validates :source, 'validates/source' => true, allow_blank: true, unless: :ignore_configurable_valid_if
+  validates :rank, presence: true, unless: :ignore_configurable_valid_if
 
   before_save :handle_country
 
