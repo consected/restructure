6cbfc188ad0bfbe5160893d08579c50f5aa52f36
Phil <phil.ayres@consected.com>
2026-03-04 18:40:27 +0000

    Fixed test setup

--- app-scripts/clean-test-db.sh
+++ app-scripts/clean-test-db.sh
@@ -8,5 +8,5 @@ export DBNUM
 echo "Cleaning a single test database (number: ${DBNUM} and test environment code: ${TEST_ENV_SET}) with user Postgres user: ${USE_PG_UNAME}"
 app-scripts/drop-test-db.sh 1 && \
   app-scripts/create-test-db.sh 1
-rm -rf /var/tmp/nfs_store_tmp*
-rm -rf /var/tmp/nfs_store_test*
+rm -rf /var/tmp/nfs_store_tmp${TEST_ENV_NUMBER}${TEST_ENV_SET} && \
+  rm -rf /var/tmp/nfs_store_test${TEST_ENV_NUMBER}${TEST_ENV_SET}
