8e81e75f1336484e9642114afb4c37bd69b14674
Phil <phil.ayres@consected.com>
2026-03-06 10:14:40 +0000

    Added set_variables save trigger - resolves #964

--- app/models/dynamic/implementation_handler.rb
+++ app/models/dynamic/implementation_handler.rb
@@ -16,7 +16,8 @@ module Dynamic
 
       # skip_save_trigger: Prevent save triggers from running
       # save_trigger_results: Results from stored locally by save triggers
-      attr_accessor :skip_save_trigger, :save_trigger_results, :option_type
+      # trigger_variables: Variables set by the set_variables save trigger
+      attr_accessor :skip_save_trigger, :save_trigger_results, :trigger_variables, :option_type
     end
 
     class_methods do
