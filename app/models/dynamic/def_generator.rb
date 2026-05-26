c9d652b14e6b9e134deb37258e1c1ce3d594b370
Phil <phil.ayres@consected.com>
2026-05-26 14:20:12 +0100

    Fixed apply_encrypted_attributes for NamedConfiguration db_columns and updated nfs_store notify spec - refs #1058 #986

--- app/models/dynamic/def_generator.rb
+++ app/models/dynamic/def_generator.rb
@@ -333,7 +333,14 @@ module Dynamic
     def apply_encrypted_attributes(impl_class)
       return if db_columns.blank?
 
-      encrypted_fields = db_columns.select { |_field, config| config.is_a?(Hash) && config[:encrypted] }
+      # db_columns values may be plain Hashes (legacy) or NamedConfiguration
+      # objects (post-BaseConfiguration refactor). Both respond to [].
+      encrypted_fields = db_columns.select do |_field, config|
+        next false if config.nil?
+        next config[:encrypted] if config.respond_to?(:[])
+
+        false
+      end
       return if encrypted_fields.empty?
 
       encrypted_fields.each_key do |field_name|
