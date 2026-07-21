d9692e5dd55f273c6d1fb13f9cd21348f82fbdc8
Phil Ayres <phil_ayres@hms.harvard.edu>
2026-07-21 09:57:21 +0100

    Changed parallel test scripts to limit processes and improve DB setup

--- app-scripts/clean-test-db.sh
+++ app-scripts/clean-test-db.sh
@@ -6,7 +6,7 @@ export USE_PG_UNAME=${USE_PG_UNAME:=$(whoami)}
 DBNUM=${DBNUM:=${TEST_ENV_NUMBER}}
 export DBNUM
 echo "Cleaning a single test database (number: ${DBNUM} and test environment code: ${TEST_ENV_SET}) with user Postgres user: ${USE_PG_UNAME}"
-app-scripts/drop-test-db.sh 1 && \
-  app-scripts/create-test-db.sh 1
-rm -rf /var/tmp/nfs_store_tmp${TEST_ENV_NUMBER}${TEST_ENV_SET} && \
-  rm -rf /var/tmp/nfs_store_test${TEST_ENV_NUMBER}${TEST_ENV_SET}
+app-scripts/drop-test-db.sh 1
+app-scripts/create-test-db.sh 1
+rm -rf /var/tmp/nfs_store_tmp${TEST_ENV_NUMBER}${TEST_ENV_SET}
+rm -rf /var/tmp/nfs_store_test${TEST_ENV_NUMBER}${TEST_ENV_SET}
