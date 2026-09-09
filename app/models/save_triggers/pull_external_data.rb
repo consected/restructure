c41e8e895b7b032cdb6bfeb33405cb5f743317f7
Phil Ayres <phil_ayres@hms.harvard.edu>
2026-09-09 14:53:52 +0100

    Added pull external response headers - fixes #1430

--- app/models/save_triggers/pull_external_data.rb
+++ app/models/save_triggers/pull_external_data.rb
@@ -50,15 +50,7 @@ class SaveTriggers::PullExternalData < SaveTriggers::SaveTriggersBase
           end
 
           vals[response_code_field] = response_code if response_code_field
-          if local_data_name
-            @item.save_trigger_results[local_data_name] = orig_data
-            @item.save_trigger_results["#{local_data_name}_http_response_code"] = response_code
-            @item.save_trigger_results["#{local_data_name}_submitted_request"] = {
-              'data' => @submitted_request_data,
-              'url' => url_from_config,
-              'method' => method_from_config
-            }
-          end
+          store_local_data_results(local_data_name, orig_data)
 
           # We calculate the conditional if inside each item, rather than relying
           # on the outer processing in ActivityLogOptions#calc_save_trigger_if
@@ -81,21 +73,7 @@ class SaveTriggers::PullExternalData < SaveTriggers::SaveTriggersBase
           next unless vals.present?
 
           raise_if_in_before_save_trigger!
-
-          # Retain the flags so that the #update! doesn't change
-          # what we need to report through the API
-          res = @item
-          created = res._created
-          updated = res._updated
-          disabled = res._disabled
-          @item.transaction do
-            res.ignore_configurable_valid_if = true if config[:force_not_valid]
-            res.force_save! if config[:force_not_editable_save]
-            res.update! vals.merge(current_user: @item.current_user || @item.user, skip_save_trigger: true)
-          end
-          res._created = created
-          res._updated = updated
-          res._disabled = disabled
+          update_item(vals, config)
         end
       end
     end
@@ -165,6 +143,7 @@ class SaveTriggers::PullExternalData < SaveTriggers::SaveTriggersBase
     format = sub_config[:format]
 
     self.response_code = response.code.to_i
+    store_response_headers(@this_config[:local_data], response_headers(response))
     content = response.body
     allowed_non_success_response = response_code != 200 && response_code.in?(allow_response_codes)
 
@@ -266,6 +245,48 @@ class SaveTriggers::PullExternalData < SaveTriggers::SaveTriggersBase
     raise unless fallback_to_raw
 
     content
+  end
+
+  def response_headers(response)
+    response.each_header.with_object({}) do |(name, value), headers|
+      headers[name] = value
+      headers[name.id_underscore.to_sym] = value
+    end
+  end
+
+  def store_response_headers(local_data_name, headers)
+    return unless local_data_name && headers
+
+    @item.save_trigger_results["#{local_data_name}_http_response_headers"] = headers
+  end
+
+  def store_local_data_results(local_data_name, data)
+    return unless local_data_name
+
+    @item.save_trigger_results[local_data_name] = data
+    @item.save_trigger_results["#{local_data_name}_http_response_code"] = response_code
+    @item.save_trigger_results["#{local_data_name}_submitted_request"] = {
+      'data' => @submitted_request_data,
+      'url' => url_from_config,
+      'method' => method_from_config
+    }
+  end
+
+  def update_item(vals, config)
+    # Retain the flags so that the #update! doesn't change
+    # what we need to report through the API
+    res = @item
+    created = res._created
+    updated = res._updated
+    disabled = res._disabled
+    @item.transaction do
+      res.ignore_configurable_valid_if = true if config[:force_not_valid]
+      res.force_save! if config[:force_not_editable_save]
+      res.update! vals.merge(current_user: @item.current_user || @item.user, skip_save_trigger: true)
+    end
+    res._created = created
+    res._updated = updated
+    res._disabled = disabled
   end
 
   #
